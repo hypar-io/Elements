30fabdaa92b99d5b26de09aeea53ab6577f2cea9
Ian Keough <ian@iankeough.com>
2018-06-29 16:12:10 -0700

    Add a deployment script for the CLI.

---
+++ deploy_sdk.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+dotnet publish ./src/cli/hypar-cli.csproj -c Release -r linux-x64 &&
+dotnet publish ./src/cli/hypar-cli.csproj -c Release -r osx.10.12-x64 &&
+dotnet publish ./src/cli/hypar-cli.csproj -c Release -r win-x64 &&
+
+zip -r hypar-linux-x64.zip -j ./src/cli/bin/Release/netcoreapp2.1/linux-x64/publish &&
+zip -r hypar-osx.10.12-x64.zip -j ./src/cli/bin/Release/netcoreapp2.1/osx.10.12-x64/publish &&
+zip -r hypar-win-x64.zip -j ./src/cli/bin/Release/netcoreapp2.1/win-x64/publish &&
+
+aws s3 cp hypar-linux-x64.zip s3://hypar-cli
+aws s3 cp hypar-osx.10.12-x64.zip s3://hypar-cli
+aws s3 cp hypar-win-x64.zip s3://hypar-cli
+
+rm hypar-linux-x64.zip
+rm hypar-osx.10.12-x64.zip
+rm hypar-win-x64.zip
