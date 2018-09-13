ee154e2c42ca4a293d8b301f5796cb1a57ba2faf
Anthony Hauck <33582525+hummingburd@users.noreply.github.com>
2018-09-13 06:01:58 -0400

    Forgot to initialize the height

--- csharp/src/sdk/Elements/Space.cs
+++ csharp/src/sdk/Elements/Space.cs
@@ -69,6 +69,7 @@ namespace Hypar.Elements
 
             this.Location = bottom;
             this.BottomElevation = bottomElevation;
+            this.Height = height;
             this.Material = BuiltInMaterials.Default;
         }
 
