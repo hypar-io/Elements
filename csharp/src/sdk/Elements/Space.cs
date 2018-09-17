dc0d2c6f6579ab7ff2afc07a0dacbc083c42e872
Ian Keough <ian@hypar.io>
2018-09-17 06:45:57 -0700

    Small space cleanups after merge.

--- csharp/src/sdk/Elements/Space.cs
+++ csharp/src/sdk/Elements/Space.cs
@@ -10,7 +10,7 @@ namespace Hypar.Elements
     /// <summary>
     /// A space represents the extruded boundary of an occupiable region.
     /// </summary>
-    public class Space : Element, ILocateable<Polyline>, ITessellate<Mesh>, ITransformable, IMaterialize
+    public class Space : Element, ILocateable<Polygon>, ITessellate<Mesh>
     {
         /// <summary>
         /// The elevation of the lower Space perimeter.
@@ -29,31 +29,17 @@ namespace Hypar.Elements
         /// </summary>
         /// <returns></returns>
         [JsonProperty("location")]
-        public Polyline Location{get;}
-
-        /// <summary>
-        /// The material of the Space.
-        /// </summary>
-        /// <value></value>
-        [JsonIgnore]
-        public Material Material { get; set; }
-
-        /// <summary>
-        /// The transform of the Space.
-        /// </summary>
-        /// <value></value>
-        [JsonProperty("transform")]
-        public Transform Transform { get; }
+        public Polygon Location{get;}
 
         /// <summary>
         /// The voids within the Space.
         /// </summary>
         /// <value></value>
         [JsonProperty("voids")]
-        public IEnumerable<Polyline> Voids { get; }
+        public IEnumerable<Polygon> Voids { get; }
 
         /// <summary>
-        /// Construct a default Space.
+        /// Construct a space.
         /// </summary>
         public Space()
         {
@@ -65,14 +51,14 @@ namespace Hypar.Elements
         }
 
         /// <summary>
-        /// Construct a Space from a perimeter, a list of voids, a lower elevation, and a height.
+        /// Construct a space.
         /// </summary>
-        /// <param name="perimeter">The lower perimeter of the space.</param>
+        /// <param name="perimeter">The perimeter of the space.</param>
         /// <param name="voids">A list of perimeters as vertical voids the same height as the space.</param>
-        /// <param name="elevation">The elevation of the lower perimeter.</param>
+        /// <param name="elevation">The elevation of the perimeter.</param>
         /// <param name="height">The height of the space above the lower elevation.</param>
         /// <returns></returns>
-        public Space(Polyline perimeter, IEnumerable<Polyline> voids, double elevation, double height)
+        public Space(Polygon perimeter, IEnumerable<Polygon> voids, double elevation, double height)
         {
             if (height <= 0.0)
             {
@@ -96,7 +82,7 @@ namespace Hypar.Elements
         /// <returns></returns>
         public Mesh Tessellate()
         {
-            var polys = new List<Polyline>
+            var polys = new List<Polygon>
             {
                 this.Location
             };
