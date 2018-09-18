a0fbd2d2e4bbef270e970672a453a8153873c6fb
Anthony Hauck <33582525+hummingburd@users.noreply.github.com>
2018-09-18 09:08:32 -0400

    My mistake for listening to VS

--- csharp/test/Hypar.SDK.Tests/PolygonTests.cs
+++ csharp/test/Hypar.SDK.Tests/PolygonTests.cs
@@ -300,7 +300,7 @@ namespace Hypar.Tests
 
             var plinew = new Polygon(new[]{a,b,c});
             var offset = plinew.Offset(0.2);
-            Assert.Single(offset);
+            Assert.True(offset.Count() == 1);
         }
 
         [Fact]
