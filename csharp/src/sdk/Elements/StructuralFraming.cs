6ee66a0cb0fd4a069cc868971b889ebe3e73fec5
Ian Keough <ian@hypar.io>
2018-10-01 14:44:00 -0700

    Add setbacks to structural framing.

--- csharp/src/sdk/Elements/StructuralFraming.cs
+++ csharp/src/sdk/Elements/StructuralFraming.cs
@@ -61,20 +61,42 @@ namespace Hypar.Elements
         }
 
         /// <summary>
+        /// The setback of the beam's extrusion at the start.
+        /// </summary>
+        [JsonProperty("start_setback")]
+        public double StartSetback{get;}
+
+        /// <summary>
+        /// Thet setback of the Beam's extrusion at the end.
+        /// </summary>
+        [JsonProperty("end_setback")]
+        public double EndSetback{get;}
+
+        /// <summary>
         /// Construct a beam.
         /// </summary>
         /// <param name="centerLine">The center line of the Beam.</param>
         /// <param name="profile">The structural Profile of the Beam.</param>
         /// <param name="material">The Beam's material.</param>
         /// <param name="up">The up axis of the Beam.</param>
+        /// <param name="startSetback">The setback of the framing's extrusion at its start.</param>
+        /// <param name="endSetback">The setback of the framing's extrusion at its end.</param>
         [JsonConstructor]
-        public StructuralFraming(Line centerLine, Profile profile, Material material = null, Vector3 up = null)
+        public StructuralFraming(Line centerLine, Profile profile, Material material = null, Vector3 up = null, double startSetback = 0.0, double endSetback = 0.0)
         {
             this._profile = profile;
             this._centerLine = centerLine;
             this.Material = material == null ? BuiltInMaterials.Steel : material;
             var t = centerLine.GetTransform(0.0, up);
             this.UpAxis = up == null ? t.YAxis : up;
+
+            var l = centerLine.Length;
+            if(startSetback > l || endSetback > l)
+            {
+                throw new ArgumentOutOfRangeException($"The start and end setbacks ({startSetback},{endSetback}) must be less than the length of the beam ({l}).");
+            }
+            this.StartSetback = startSetback;
+            this.EndSetback = endSetback;
         }
 
         /// <summary>
@@ -83,7 +105,7 @@ namespace Hypar.Elements
         /// <returns>A mesh representing the tessellated Beam.</returns>
         public Mesh Mesh()
         {
-            return Hypar.Geometry.Mesh.ExtrudeAlongLine(this._centerLine, this._profile.Perimeter, this._profile.Voids);
+            return Hypar.Geometry.Mesh.ExtrudeAlongLine(this._centerLine, this._profile.Perimeter, this._profile.Voids, true, this.StartSetback, this.EndSetback);
         }
 
         /// <summary>
@@ -93,6 +115,8 @@ namespace Hypar.Elements
         public IList<IList<Vector3>> Curves()
         {
             var curves = new List<IList<Vector3>>();
+            curves.Add(this.CenterLine.Vertices);
+
             var prof = this.CenterLine.GetTransform(0.0).OfProfile(this.Profile);
 
             curves.Add(prof.Perimeter.Vertices);
@@ -105,7 +129,6 @@ namespace Hypar.Elements
             {
                 curves.Add(v.Vertices);
             }
-
             return curves;
         }
     }
--- csharp/test/Hypar.SDK.Tests/StructuralFramingTests.cs
+++ csharp/test/Hypar.SDK.Tests/StructuralFramingTests.cs
@@ -12,7 +12,7 @@ namespace Hypar.Tests
         public void Example()
         {
             var line = new Line(Vector3.Origin, new Vector3(2,0,0));
-            var beam = new Beam(line, WideFlangeProfileServer.Instance.GetProfileByName("W44x335"), BuiltInMaterials.Steel);
+            var beam = new Beam(line, WideFlangeProfileServer.Instance.GetProfileByName("W44x335"), BuiltInMaterials.Steel, null, 0.05, 0.05);
             var model = new Model();
             model.AddElement(beam);
             model.SaveGlb("beam.glb");
