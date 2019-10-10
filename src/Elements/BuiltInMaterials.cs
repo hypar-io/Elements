421d8ef5f1c40b6f8153b72795d71d3deb7e2aa7
Ian Keough <ian@hypar.io>
2019-10-09 23:54:24 -0700

    Fix the built in axes materials.

--- src/Elements/BuiltInMaterials.cs
+++ src/Elements/BuiltInMaterials.cs
@@ -18,9 +18,9 @@ namespace Elements
         private static Material _topography = new Material("topography", new Color(0.59f, 0.59f, 0.39f, 1.0f), 0.0f, 0.0f, true);
         private static Material _edgesHighlighted = new Material("edge_highlighted", new Color(1.0f, 1.0f, 0.0f, 1.0f), 0.0f, 0.0f);
         private static Material _void = new Material("void", new Color(Colors.Lime.Red, Colors.Lime.Green, Colors.Lime.Blue, 0.1f), 0.1f, 0.1f);
-        private static Material _xAxis = new Material("x_axis", new Color(1.0f, 0.0f, 0.0f, 1.0f), 0.1f, 0.1f);
-        private static Material _yAxis = new Material("x_axis", new Color(0.0f, 1.0f, 0.0f, 1.0f), 0.1f, 0.1f);
-        private static Material _zAxis = new Material("x_axis", new Color(0.0f, 0.0f, 1.0f, 1.0f), 0.1f, 0.1f);
+        private static Material _xAxis = new Material("X", new Color(1.0f, 0.0f, 0.0f, 1.0f), 0.1f, 0.1f);
+        private static Material _yAxis = new Material("Y", new Color(0.0f, 1.0f, 0.0f, 1.0f), 0.1f, 0.1f);
+        private static Material _zAxis = new Material("Z", new Color(0.0f, 0.0f, 1.0f, 1.0f), 0.1f, 0.1f);
         private static Material _trans = new Material("trans", new Color(0.0f,0.0f,0.0f,0.0f),0.0f,0.0f);
 
         /// <summary>
