256f5ff563685063703483e8f10a6f86da33dfb8
Ian Keough <ian@hypar.io>
2019-10-24 10:24:32 -0700

    Put the transform back, but don't adjust it in the constructor.

--- src/Elements/Opening.cs
+++ src/Elements/Opening.cs
@@ -30,14 +30,16 @@ namespace Elements
         /// <param name="y">The distance along the y axis to the center of the opening.</param>
         /// <param name="width">The width of the opening.</param>
         /// <param name="height">The height of the opening.</param>
+        /// <param name="transform">The opening's transform.</param>
         /// <param name="id">The id of the opening..</param>
         /// <param name="name">The name of the opening.</param>
         public Opening(double x,
                        double y,
                        double width,
                        double height,
+                       Transform transform = null,
                        Guid id = default(Guid),
-                       string name = null) : base(id, name)
+                       string name = null) : base(id, name, transform)
         {
             this.Profile = new Profile(Polygon.Rectangle(width, height));
             this.Profile.Transform(new Transform(new Vector3(x, y)));
@@ -49,13 +51,15 @@ namespace Elements
         /// <param name="perimeter">A polygon representing the perimeter of the opening.</param>
         /// <param name="x">The distance along the x to transform the profile.</param>
         /// <param name="y">The distance along the y to transform the profile.</param>
+        /// <param name="transform">The opening's transform.</param>
         /// <param name="id">The id of the opening.</param>
         /// <param name="name">The name of the opening.</param>
         public Opening(Polygon perimeter,
                        double x = 0.0,
                        double y = 0.0,
+                       Transform transform = null,
                        Guid id = default(Guid),
-                       string name = null) : base(id, name)
+                       string name = null) : base(id, name, transform)
         {
             this.Profile = perimeter;
             this.Profile.Transform(new Transform(new Vector3(x, y)));
@@ -66,7 +70,7 @@ namespace Elements
         /// </summary>
         /// <param name="profile">A polygon representing the perimeter of the opening.</param>
         /// <param name="depth">The depth of the opening's extrusion.</param>
-        /// <param name="transform">An additional transform applied to the opening.</param>
+        /// <param name="transform">The opening's transform.</param>
         /// <param name="id">The id of the opening.</param>
         /// <param name="name">The name of the opening.</param>
         [JsonConstructor]
@@ -89,7 +93,7 @@ namespace Elements
             {
                 return;
             }
-            
+
             // TODO(Ian): Give this a proper depth when booleans are supported.
             this.Geometry.SolidOperations.Add(new Extrude(this.Profile, 5, this.Transform.ZAxis, 0.0, true));
         }
