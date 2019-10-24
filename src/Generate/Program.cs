6ca5e0c08aa53a8df02f081e054f352130f263de
Ian Keough <ian@hypar.io>
2019-10-24 14:26:07 -0700

    Add schema generation from urls.

--- src/Generate/Program.cs
+++ src/Generate/Program.cs
@@ -25,6 +25,51 @@ namespace Elements.Generate
                     "Position", "Elements", "GeometricElement", "Geometry",
                     "Identifiable", "SolidOperation", "Representation"};
 
+            await WriteTypesFromUrls(outRoot, excludedTypes);
+        }
+
+        private static async Task WriteTypesFromUrls(string outRoot, string[] excludedTypes = null)
+        {
+            var urls = new string[]{
+                "https://hypar.io/Schemas/GeoJSON/Position.json",
+                "https://hypar.io/Schemas/Geometry/Solids/Extrude.json",
+                "https://hypar.io/Schemas/Geometry/Solids/Lamina.json",
+                "https://hypar.io/Schemas/Geometry/Solids/SolidOperation.json",
+                "https://hypar.io/Schemas/Geometry/Solids/Sweep.json",
+                "https://hypar.io/Schemas/Geometry/Arc.json",
+                "https://hypar.io/Schemas/Geometry/Color.json",
+                "https://hypar.io/Schemas/Geometry/Curve.json",
+                "https://hypar.io/Schemas/Geometry/Line.json",
+                "https://hypar.io/Schemas/Geometry/Plane.json",
+                "https://hypar.io/Schemas/Geometry/Polygon.json",
+                "https://hypar.io/Schemas/Geometry/Polyline.json",
+                "https://hypar.io/Schemas/Geometry/Profile.json",
+                "https://hypar.io/Schemas/Geometry/Representation.json",
+                "https://hypar.io/Schemas/Geometry/Transform.json",
+                "https://hypar.io/Schemas/Geometry/Vector3.json",
+                "https://hypar.io/Schemas/Properties/NumericProperty.json",
+                "https://hypar.io/Schemas/Element.json",
+                "https://hypar.io/Schemas/GeometricElement.json",
+                "https://hypar.io/Schemas/Identifiable.json",
+                "https://hypar.io/Schemas/Material.json",
+                "https://hypar.io/Schemas/Model.json"
+            };
+            foreach(var url in urls)
+            {
+                var split = url.Split("/", StringSplitOptions.RemoveEmptyEntries).Skip(3);
+                var ns = $"Elements.{string.Join('.', split)}";
+                var outDir = Path.Combine(outRoot, string.Join('/', split.SkipLast(1)));
+                if(!Directory.Exists(outDir))
+                {
+                    Directory.CreateDirectory(outDir);
+                }
+                var outPath = Path.Combine(outDir, split.Last().Replace(".json",".g.cs"));
+                await WriteTypesFromSchemasOnline(url, outPath, ns, excludedTypes);
+            }
+        }
+
+        private static async Task WriteTypesFromDisk(string schemaRoot, string outRoot, string[] excludedTypes = null)
+        {
             var di = new DirectoryInfo(schemaRoot);
             foreach(var fi in di.EnumerateFiles("*.json", SearchOption.AllDirectories))
             {
@@ -40,11 +85,28 @@ namespace Elements.Generate
                 }
 
                 var outPath = Path.Combine(outDir, fi.Name.Replace(".json",".g.cs"));
-                await WriteTypes(fi.FullName, outPath, ns, excludedTypes.Where(t=>t != fi.Name.Replace(".json","")).ToArray());
+                await WriteTypesFromSchemasOnDisk(fi.FullName, outPath, ns, excludedTypes.Where(t=>t != fi.Name.Replace(".json","")).ToArray());
             }
         }
 
-        private static async Task WriteTypes(string schemaPath, string outPath, string ns, string[] excludedTypes = null)
+        private static async Task WriteTypesFromSchemasOnline(string schemaUrl, string outPath, string ns, string[] excludedTypes = null)
+        {
+            Console.WriteLine($"Generating type in {outPath}...");
+            var schema = await JsonSchema.FromUrlAsync(schemaUrl);
+            var generator = new CSharpGenerator(schema, new CSharpGeneratorSettings(){
+                Namespace = ns, 
+                ArrayType = "IList",
+                ArrayInstanceType = "List",
+                ExcludedTypeNames = excludedTypes == null ? new string[]{} : excludedTypes,
+                TemplateDirectory = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "../../../Templates"),
+                GenerateJsonMethods = false,
+                ClassStyle = CSharpClassStyle.Poco
+            });
+            var file = generator.GenerateFile();
+            File.WriteAllText(outPath, file);
+        }
+
+        private static async Task WriteTypesFromSchemasOnDisk(string schemaPath, string outPath, string ns, string[] excludedTypes = null)
         {
             Console.WriteLine($"Generating types in {outPath}...");
             var schema = await JsonSchema.FromJsonAsync(File.ReadAllText(schemaPath), schemaPath);
