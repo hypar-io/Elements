9c75d4d0918ddd4a1bb64d8f5320ab71ffe19707
Ian Keough <ian@hypar.io>
2019-11-12 15:44:08 -0800

    Fix #206.

--- src/Elements/Geometry/Solids/Sweep.cs
+++ src/Elements/Geometry/Solids/Sweep.cs
@@ -2,15 +2,13 @@ namespace Elements.Geometry.Solids
 {
     public partial class Sweep
     {
-        private double _rotation = 0.0;
-
         /// <summary>
         /// Get the updated solid representation of the frame.
         /// </summary>
         /// <returns></returns>
         internal override Solid GetSolid()
         {
-            return Kernel.Instance.CreateSweepAlongCurve(this.Profile, this.Curve, this.StartSetback, this.EndSetback, _rotation);
+            return Kernel.Instance.CreateSweepAlongCurve(this.Profile, this.Curve, this.StartSetback, this.EndSetback, this.Rotation);
         }
     }
 }
