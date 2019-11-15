b40c0752e6886563e0273feabc1488317de2ad61
Ian Keough <ian@hypar.io>
2019-11-15 14:37:50 -0800

    Test user elements with sub elements.

--- test/Elements.Tests/UserElementTests.cs
+++ test/Elements.Tests/UserElementTests.cs
@@ -12,7 +12,10 @@ namespace Elements.Tests
     public class TestUserElement : GeometricElement
     {
         public Line CenterLine { get; set; }
+
         public Profile Profile { get; set; }
+
+        public List<Element> SubElements { get; set; }
 
         /// <summary>
         /// Something you want to measure.
@@ -31,6 +34,7 @@ namespace Elements.Tests
         {
             this.CenterLine = centerLine;
             this.Profile = profile;
+            this.SubElements = new List<Element>();
 
             var t = this.CenterLine.TransformAt(0);
             var x = new Line(t.Origin, t.XAxis * this.CenterLine.Length());
@@ -38,14 +42,6 @@ namespace Elements.Tests
             this.Representation.SolidOperations.Add(new Sweep(this.Profile, this.CenterLine, 0.0, 0.0, 0.0, false));
             this.Representation.SolidOperations.Add(new Sweep(this.Profile, x,  0.0, 0.0, 0.0, false));
             this.Representation.SolidOperations.Add(new Sweep(this.Profile, y,  0.0, 0.0, 0.0, false));
-        }
-
-        /// <summary>
-        /// Update the representations.
-        /// </summary>
-        public override void UpdateRepresentations()
-        {
-            return;
         }
     }
 
@@ -58,15 +54,24 @@ namespace Elements.Tests
 
             var line = new Line(Vector3.Origin, new Vector3(5, 5, 5));
             var ue = new TestUserElement(line, new Profile(Polygon.L(1, 2, 0.5)));
-            this.Model.AddElement(ue);
+            
+            var p = new Profile(Polygon.Rectangle(1,1));
+            var m1 = new Mass(p, 1);
+            var m2 = new Mass(p, 1);
+            ue.SubElements.Add(m1);
+            ue.SubElements.Add(m2);
 
-            // var beam = new Beam(line, new Profile(Polygon.Rectangle(0.25, 0.25)), BuiltInMaterials.Steel);
-            // this.Model.AddElement(beam);
+            this.Model.AddElement(ue);
 
             var json = this.Model.ToJson();
             var newModel = Model.FromJson(json);
-            Assert.Equal(3, newModel.Elements.Count);
-            Assert.Equal(1, newModel.AllElementsOfType<TestUserElement>().ToArray().Count());
+
+            Assert.Equal(7, newModel.Elements.Count);
+            Assert.Equal(1, newModel.AllElementsOfType<TestUserElement>().Count());
+
+            // Two profiles. The one for the user element
+            // and the one for the sub-element masses.
+            Assert.Equal(2, newModel.AllElementsOfType<Profile>().Count());
         }
     }
 }
