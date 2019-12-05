3e96a8ca6ec3c5f57069efe10d4ed709edbd6bff
Ian Keough <ian@hypar.io>
2019-12-05 15:17:00 -0800

    Add a wall test to ensure consistent transforms.

--- test/Elements.Tests/WallTests.cs
+++ test/Elements.Tests/WallTests.cs
@@ -51,5 +51,38 @@ namespace Elements.Tests
             var line = new Line(a, b);
             Assert.Throws<ArgumentException>(() => new StandardWall(line, 0.1, 5.0));
         }
+
+        [Fact]
+        public void TransformedAndSerializedWalls()
+        {
+            this.Name = "TransformedSerializedWalls";
+
+            var a = Vector3.Origin;
+            var b = new Vector3(0, 5.0, 0.0);
+            var line = new Line(a,b);
+            var wall1 = new StandardWall(line, 0.1, 3.0, BuiltInMaterials.Mass);
+            this.Model.AddElement(wall1);
+
+            var c = new Vector3(0, 0, 3.0);
+            var d = new Vector3(0, 5, 3.0);
+            var line1 = new Line(c,d);
+            var wall2 = new StandardWall(line1, 0.1, 3.0, BuiltInMaterials.Void);
+            wall2.Transform.Rotate(Vector3.ZAxis, 45);
+            this.Model.AddElement(wall2);
+
+            var json = this.Model.ToJson();
+
+            var newModel = Model.FromJson(json);
+
+            // Add the new walls back to the original model
+            // and transform them
+            var walls = newModel.AllElementsOfType<StandardWall>();
+            foreach(var w in walls)
+            {
+                w.Id = Guid.NewGuid();
+                w.Transform.Move(new Vector3(5,0));
+                this.Model.AddElement(w);
+            }
+        }
     }
 }
