3354654cd330257509b945ff59bb5a550281805b
Eric Wassail <eric.wassail@gmail.com>
2020-02-12 18:30:26 -0500

    use generated  WallByProfile element type

--- src/Revit/RevitHyparTools/WallByProfile.cs
+++ src/Revit/RevitHyparTools/WallByProfile.cs
@@ -5,36 +5,29 @@ using Elements;
 using Elements.Geometry;
 using Elements.Geometry.Solids;
 
-namespace RevitHyparTools {
-    [UserElement]
-    public class WallByProfile : Wall
+namespace Elements {
+    public partial class WallByProfile 
     {
-        public Line CenterLine {get;}
-        public double Thickness {get;}
-        public WallByProfile(Line centerLine,
-                             Profile profile,
-                             double thickness,
-                             Material material = null,
-                             Transform transform = null,
-                             Representation representation = null,
-                             bool isElementDefinition = false,
-                             Guid id = default(Guid),
-                             string name = null) : base(transform != null ? transform : new Transform(),
-                                                 material != null ? material : BuiltInMaterials.Concrete,
-                                                 representation != null ? representation : new Representation(new List<SolidOperation>()),
-                                                 isElementDefinition,
-                                                 id != default(Guid) ? id : Guid.NewGuid(),
-                                                 name)
-        {
-            Profile = profile;
-            CenterLine = centerLine;
-            Thickness = thickness;
-        }
+        public WallByProfile(Profile @profile, double @thickness, Line @centerline, Transform @transform=null, Material @material=null, Representation @representation=null, bool @isElementDefinition=false)
+            : base(transform != null ? transform : new Transform(),
+                   material != null ? material : BuiltInMaterials.Default,
+                   representation != null ? representation: new Representation(new List<SolidOperation>()),
+                   isElementDefinition,
+                   Guid.NewGuid() ,
+                   "Wall by Profile") {
+
+            this.Profile = @profile;
+            this.Thickness = @thickness;
+            this.Centerline = @centerline;
+                   }
 
         public override void UpdateRepresentations() {
             this.Representation.SolidOperations.Clear();
             // The wall is a simple extrusion of the profile
-            this.Representation.SolidOperations.Add(new Extrude(this.Profile, this.Thickness, Profile.Perimeter.Normal(), false));
+            // var midpoint = Centerline.PointAt(0.5);
+            // var direction = new Line(midpoint, midpoint.Project(Profile.Perimeter.Plane())).Direction();
+            var direction = Profile.Perimeter.Normal();
+            this.Representation.SolidOperations.Add(new Extrude(this.Profile, this.Thickness, direction, false));
         }
     }
 }
