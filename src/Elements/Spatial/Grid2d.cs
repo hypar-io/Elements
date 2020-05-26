cfc3de43006062bd209324ddc6e9a10305b91261
Andrew Heumann <andrew@heumann.com>
2020-05-26 07:48:14 -0400

    remove unused usings

--- src/Elements/Spatial/Grid2d.cs
+++ src/Elements/Spatial/Grid2d.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Elements.Geometry;
 using Elements.Geometry.Interfaces;
-using IFC;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Newtonsoft.Json;
 
 namespace Elements.Spatial
