23d42621c19926ac110f5db8ba37cfcedfbbce39
Eric Wassail <eric.wassail@gmail.com>
2020-06-22 18:00:01 -0400

    simpler Area geom extraction

--- src/Revit/RevitHyparTools/Create.SpaceBoundary.cs
+++ src/Revit/RevitHyparTools/Create.SpaceBoundary.cs
@@ -14,11 +14,10 @@ namespace Hypar.Revit
     {
         public static SpaceBoundary[] SpaceBoundaryFromRevitArea(ADSK.Area area, Document doc)
         {
-            var geom = area.get_Geometry(new Options()
-            {
-                View = GetViewWhereElemIsVisible(doc, area)
-            });
-            var face = geom.Where(g => g.GetType() == typeof(ADSK.Solid)).Cast<ADSK.Solid>().Where(g => g != null).First().Faces.get_Item(0) as PlanarFace;
+            var spatialElementCalculator = new SpatialElementGeometryCalculator(doc);
+            var geometryCalculationResult = spatialElementCalculator.CalculateSpatialElementGeometry(area);
+            var geom = geometryCalculationResult.GetGeometry();
+            var face = geom.Faces.get_Item(0) as PlanarFace;
 
             var boundaries = new List<SpaceBoundary>();
             foreach (var p in face.GetProfiles(true))
@@ -33,19 +32,6 @@ namespace Hypar.Revit
                 boundaries.Add(boundary);
             }
             return boundaries.ToArray();
-        }
-
-        private static View GetViewWhereElemIsVisible(Document doc, ADSK.Element elem)
-        {
-            var col = new FilteredElementCollector(doc).WhereElementIsNotElementType().OfClass(typeof(ViewPlan)).Cast<ViewPlan>().Where(e => !e.IsTemplate);
-            foreach (var view in col)
-            {
-                if (new FilteredElementCollector(doc, view.Id).ToElementIds().Contains(elem.Id))
-                {
-                    return view as View;
-                }
-            }
-            return null;
         }
     }
 }
