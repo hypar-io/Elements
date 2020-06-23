0b5c85fc67094ff4de1e30adb1845f0e856124ba
Eric Wassail <eric.wassail@gmail.com>
2020-06-23 11:41:39 -0400

    fix and generation documentation

--- src/Dynamo/src/HyparDynamo/Hypar.cs
+++ src/Dynamo/src/HyparDynamo/Hypar.cs
@@ -13,31 +13,33 @@ using Autodesk.Revit.DB;
 
 namespace HyparDynamo.Hypar
 {
-    /// <summary>
-    /// Convert a Revit Area into a SpaceBoundary element. This currently only
-    /// stores the perimeter polygon, and no geometric representation, so you will 
-    /// not see it in 3D views in Hypar.
-    /// </summary>
-    /// <param name="area">The area that is meant to be converted to a Hypar SpaceBoundary.</param>
     public static class SpaceBoundary
     {
-        public static Elements.SpaceBoundary[] FromArea(Revit.Elements.Element area)
+        /// <summary>
+        /// Convert a Revit Area into a SpaceBoundary element. This currently only
+        /// stores the perimeter polygon, and no geometric representation, so you will 
+        /// not see it in 3D views in Hypar.
+        /// </summary>
+        /// <param name="revitArea">The area that is meant to be converted to a Hypar SpaceBoundary.</param>
+        /// <returns name="SpaceBoundar">The Hypar space boundary elements.</returns>
+        public static Elements.SpaceBoundary[] FromArea(Revit.Elements.Element revitArea)
         {
-            var areaElement = (Autodesk.Revit.DB.Area)area.InternalElement;
+            var areaElement = (Autodesk.Revit.DB.Area)revitArea.InternalElement;
             var doc = DocumentManager.Instance.CurrentDBDocument;
 
             return Create.SpaceBoundaryFromRevitArea(areaElement, doc);
         }
     }
 
-    /// <summary>
-    /// Convert a list of points from Dynamo into a ModelPoints element, with the option 
-    /// to add a tag that will be stored in the elements Name field.
-    /// </summary>
-    /// <param name="Points">The points that will be stored.</param>
-    /// <returns name="Tag">The tag to assign to the model points.</param>
     public static class ModelPoints
     {
+        /// <summary>
+        /// Convert a list of points from Dynamo into a ModelPoints element, with the option 
+        /// to add a tag that will be stored in the elements Name field.
+        /// </summary>
+        /// <param name="points">The points that will be stored.</param>
+        /// <param name="tag">The tag to assign to the model points.</param>
+        /// <returns name="ModelPoints">The ModelPoint objects for Hypar.</returns>
         public static Elements.ModelPoints FromPoints(List<Autodesk.DesignScript.Geometry.Point> points, string tag = "")
         {
             return Create.ModelPointsFromPoints(points.Select(p => new XYZ(p.X, p.Y, p.Z)), tag);
@@ -50,16 +52,16 @@ namespace HyparDynamo.Hypar
         /// Convert a Revit wall to Elements.WallByProfile(s) for use in Hypar models.
         /// Sometimes a single wall in Revit needs to be converted to multiple Hypar walls.
         /// </summary>
-        /// <param name="RevitWall">The walls to be exported.</param>
-        /// <returns name="WallByProfile">The Hypar walls.</param>
+        /// <param name="revitWall">The walls to be exported.</param>
+        /// <returns name="WallByProfiles">The Hypar walls.</returns>
         public static Elements.WallByProfile[] FromRevitWall(this Revit.Elements.Wall revitWall)
         {
-            var r_Wall = (Autodesk.Revit.DB.Wall)revitWall.InternalElement;
+            var revitWallElement = (Autodesk.Revit.DB.Wall)revitWall.InternalElement;
 
             // wrapped exception catching to deliver more meaningful message in Dynamo
             try
             {
-                return Create.WallsFromRevitWall(r_Wall, DocumentManager.Instance.CurrentDBDocument);
+                return Create.WallsFromRevitWall(revitWallElement, DocumentManager.Instance.CurrentDBDocument);
             }
             catch (Exception ex)
             {
@@ -75,15 +77,15 @@ namespace HyparDynamo.Hypar
         /// Sometimes a single floor in Revit needs to be converted to multiple Hypar floors.
         /// </summary>
         /// <param name="revitFloor">The floor to be exported.</param>
-        /// <returns name="floor">The Hypar floors.</param>
+        /// <returns name="Floors">The Hypar floors.</returns>
         public static Elements.Floor[] FromRevitFloor(this Revit.Elements.Floor revitFloor)
         {
-            var r_Floor = (Autodesk.Revit.DB.Floor)revitFloor.InternalElement;
+            var revitFloorElement = (Autodesk.Revit.DB.Floor)revitFloor.InternalElement;
 
             // wrapped exception catching to deliver more meaningful message in Dynamo
             try
             {
-                return Create.FloorsFromRevitFloor(DocumentManager.Instance.CurrentDBDocument, r_Floor);
+                return Create.FloorsFromRevitFloor(DocumentManager.Instance.CurrentDBDocument, revitFloorElement);
             }
             catch (Exception ex)
             {
@@ -94,12 +96,17 @@ namespace HyparDynamo.Hypar
 
     public static class Column
     {
-        public static Elements.Column FromRevitColumn(this Revit.Elements.Element column)
+        /// <summary>
+        /// Convert a column in Revit into an Elements.Column for use in Hypar models.
+        /// </summary>
+        /// <param name="revitColumn">The column to be exported.</param>
+        /// <returns name="Column">The Hypar column element.</returns>
+        public static Elements.Column FromRevitColumn(this Revit.Elements.Element revitColumn)
         {
-            var revitColumn = (Autodesk.Revit.DB.FamilyInstance)column.InternalElement;
+            var revitColumnElement = (Autodesk.Revit.DB.FamilyInstance)revitColumn.InternalElement;
             try
             {
-                return Create.ColumnFromRevitColumn(revitColumn);
+                return Create.ColumnFromRevitColumn(revitColumnElement);
             }
             catch (Exception ex)
             {
@@ -110,6 +117,11 @@ namespace HyparDynamo.Hypar
 
     public static class Model
     {
+        /// <summary>
+        /// Write a Hypar model to its JSON representation .
+        /// </summary>
+        /// <param name="filePath">The path to write the JSON file.</param>
+        /// <param name="model">The Hypar Model to write.</param>
         public static void WriteJson(string filePath, Elements.Model model)
         {
             try
@@ -122,6 +134,14 @@ namespace HyparDynamo.Hypar
                 throw new Exception(ex.Message);
             }
         }
+
+        /// <summary>
+        /// Write a Hypar model to glb.  GLB is a file format that is use for displaying 
+        /// 3D models on the web. This is the format used in the hypar.io website,
+        /// and you can also view these files using https://gltf-viewer.donmccurdy.com/ .
+        /// </summary>
+        /// <param name="filePath">The path to write the JSON file.</param>
+        /// <param name="model">The Hypar Model to write.</param>
         public static void WriteGlb(string filePath, Elements.Model model)
         {
             try
@@ -134,6 +154,11 @@ namespace HyparDynamo.Hypar
             }
         }
 
+        /// <summary>
+        /// Create a Hypar Model from a list of Elements.
+        /// </summary>
+        /// <param name="elements">The list of Elements that should be added to the model.</param>
+        /// <returns name="Model">The Hypar Model to write.</returns>
         public static Elements.Model FromElements(IList<object> elements)
         {
             var model = new Elements.Model();
