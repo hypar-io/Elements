ae2ade140162996d366429dce4113fa8154b45c7
Ian Keough <ian@hypar.io>
2020-08-06 10:37:55 -0700

    Add a basic base64 gltf test.

--- test/Elements.Tests/GltfTests.cs
+++ test/Elements.Tests/GltfTests.cs
@@ -1,4 +1,5 @@
 using Xunit;
+using Elements.Geometry;
 using Elements.Serialization.glTF;
 
 namespace Elements.Tests
@@ -10,6 +11,15 @@ namespace Elements.Tests
         {
             var model = new Model();
             model.ToGlTF("./empty_model.glb", true);
+        }
+
+        [Fact]
+        public void ModelSerializesToBase64String()
+        {
+            var model = new Model();
+            var beam = new Beam(new Line(Vector3.Origin, new Vector3(5, 5, 5)), Polygon.Rectangle(0.1, 0.2));
+            model.AddElement(beam);
+            var str = model.ToBase64String();
         }
     }
 }
