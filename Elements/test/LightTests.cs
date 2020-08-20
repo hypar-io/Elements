334873ce4f85ab68cfa3fb3183544008a68f8211
Ian Keough <ian@hypar.io>
2020-08-20 16:32:17 -0700

    Update the light tests.

--- Elements/test/LightTests.cs
+++ Elements/test/LightTests.cs
@@ -15,7 +15,7 @@ namespace Elements.Tests
             this.Model.AddElement(light);
 
             var sunMaterial = new Material("Sun", Colors.Yellow, unlit: true);
-            var dirCurve = new ModelCurve(new Line(light.Transform.Origin, light.Transform.Origin + light.Transform.ZAxis * 5.0), sunMaterial);
+            var dirCurve = new ModelCurve(new Line(light.Transform.Origin, light.Transform.Origin + light.Transform.ZAxis.Negate() * 10), sunMaterial);
             this.Model.AddElement(dirCurve);
 
             var floor = new Floor(Polygon.Rectangle(20, 20), 0.1);
@@ -23,6 +23,9 @@ namespace Elements.Tests
 
             var column = new Column(new Vector3(5, 5), 5.0, Polygon.Rectangle(0.2, 0.2));
             this.Model.AddElement(column);
+
+            var mass = new Mass(Polygon.Rectangle(1, 1), 1.0, sunMaterial, new Transform(light.Transform.Origin));
+            this.Model.AddElement(mass);
         }
     }
 }
