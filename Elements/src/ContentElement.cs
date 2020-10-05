cc3a3621e59359bf6f52b0be2078af0dd5671ffa
Eric Wassail <eric.wassail@gmail.com>
2020-10-05 11:53:27 -0400

    remove unecessary override

--- Elements/src/ContentElement.cs
+++ Elements/src/ContentElement.cs
@@ -4,9 +4,5 @@ namespace Elements
 {
     public partial class ContentElement
     {
-        public override void UpdateRepresentations()
-        {
-            var exists = File.Exists(this.GltfLocation);
-        }
     }
 }
