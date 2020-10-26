0bc7b1088f06d6d8b77e1864035f8b0a79b121af
wynged <eric.wassail@gmail.com>
2020-10-26 13:41:13 -0400

    add and refactor distanceTo methods.  more tests

--- Elements/src/Geometry/Vector3.DistanceTo.cs
+++ Elements/src/Geometry/Vector3.DistanceTo.cs
@@ -31,42 +31,65 @@ namespace Elements.Geometry
         }
 
         /// <summary>
-        /// The distance from this vector to the line.
+        /// Find the distance from this vector to the line, and output the location 
+        /// of the closest point on that line.
         /// </summary>
         /// <param name="line">The line to find the distance to.</param>
-        public double DistanceTo(Line line)
+        /// <param name="closestPoint">The point on the line that is closest to this point.</param>
+        public double DistanceTo(Line line, out Vector3 closestPoint)
         {
             var p = new Plane(this, line.Direction());
+            {
 
-            if (line.Intersects(p, out var hit))
-            {
-                // If the line intersect the plane originating at this point
-                // then the shortest distance is the distance to that intersection.
-                return this.DistanceTo(hit);
-            }
-            else
-            {
-                // If the line does not intersect that plane, then the shortest 
-                // distance is the distance from the point to the closest end of the line.
-                var distFromStart = this.DistanceTo(line.Start);
-                var distFromEnd = this.DistanceTo(line.End);
-                return distFromStart < distFromEnd ? distFromStart : distFromEnd;
+                if (line.Intersects(p, out closestPoint))
+                {
+                    // If the line intersect the plane originating at this point
+                    // then the shortest distance is the distance to that intersection.
+                    return this.DistanceTo(closestPoint);
+                }
+                else
+                {
+                    // If the line does not intersect that plane, then the shortest 
+                    // distance is the distance from the point to the closest end of the line.
+                    var distFromStart = this.DistanceTo(line.Start);
+                    var distFromEnd = this.DistanceTo(line.End);
+                    return distFromStart < distFromEnd ? distFromStart : distFromEnd;
+                }
             }
         }
 
         /// <summary>
-        /// The shortest distance from this vector to any point on the polyline.
+        /// Find the distance from this vector to the line.
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        public double DistanceTo(Line line)
+        {
+            return DistanceTo(line, out var ignoredPoint);
+        }
+
+        /// <summary>
+        /// Find the shortest distance from this vector to any point on the
+        /// polyline, and output the location of the closest point on that polyline.
         /// </summary>
         /// <param name="polyline">The polyline to </param>
+        /// <param name="closestPoint">The point on the polyline that is closest to this point.</param>
         /// <returns></returns>
-        public double DistanceTo(Polyline polyline)
+        public double DistanceTo(Polyline polyline, out Vector3 closestPoint)
         {
             var closest = double.MaxValue;
+            closestPoint = default(Vector3);
+
             foreach (var line in polyline.Segments())
             {
-                var distance = this.DistanceTo(line);
-                closest = distance < closest ? distance : closest;
+                var distance = this.DistanceTo(line, out var thisClosestPoint);
+                if (distance < closest)
+                {
+                    closest = distance;
+                    closestPoint = thisClosestPoint;
+                }
             }
+
             return closest;
         }
     }
