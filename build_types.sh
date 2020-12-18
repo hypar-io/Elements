b73c555ae120ba44c6ec23a1b8f4f2d10b7f7e04
Ian Keough <ian@hypar.io>
2020-12-17 23:03:45 -0800

    Cleanup type generator scripts.

--- build_types.sh
+++ build_types.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 BASE=$1
-BRANCH=$(git branch --show-current)
 
-dotnet run -p ./CoreTypeGenerator/CoreTypeGenerator.csproj ./Schemas ./Elements/src/Generate $1 $BRANCH
+dotnet run -p ./CoreTypeGenerator/CoreTypeGenerator.csproj ./Schemas ./Elements/src/Generate $1
