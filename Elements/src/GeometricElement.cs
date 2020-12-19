c196fa4fface2d9bf9c6efa58ca6f12ad3a2c46e
Ian Keough <ian@hypar.io>
2020-12-18 17:09:10 -0800

    A note about compatibility.

--- Elements/src/GeometricElement.cs
+++ Elements/src/GeometricElement.cs
@@ -6,6 +6,34 @@ namespace Elements
     public partial class GeometricElement
     {
         /// <summary>
+        /// The geometric element's representation.
+        /// </summary>
+        [Obsolete("Geometric elements can now store multiple representations. Use representations instead.")]
+        public SolidRepresentation Representation
+        {
+            // Geometric elements previously only stored one type of representation,
+            // a type akin to a SolidRepresentation. We serialize a solid representation,
+            // if one exists, in order to make JSON created from this version
+            // compatible with previous versions. If the incoming JSON contains
+            // a representation we do similarly, setting it to the first representation.
+            get
+            {
+                if (this.Representations != null && this.Representations.Count >= 1 && this.Representations[0].GetType() == typeof(SolidRepresentation))
+                {
+                    return (SolidRepresentation)this.Representations[0];
+                }
+                return null;
+            }
+            set
+            {
+                if (this.Representations != null && value.GetType() == typeof(SolidRepresentation))
+                {
+                    this.Representations[0] = value;
+                }
+            }
+        }
+
+        /// <summary>
         /// This method provides an opportunity for geometric elements
         /// to adjust their solid operations before tesselation. As an example,
         /// a floor might want to clip its opening profiles out of 
