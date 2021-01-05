bda2cc04b92d094725a75864c63589c4b423a491
Eric Wassail <eric.wassail@gmail.com>
2021-01-05 13:47:32 -0500

    more testy tests

--- Elements.CodeGeneration/test/CatalogGeneratorTests.cs
+++ Elements.CodeGeneration/test/CatalogGeneratorTests.cs
@@ -11,6 +11,8 @@ namespace Elements.Test
         {
             var testPath = "../../../TestData/CatalogFromRevit.json";
             CatalogGenerator.FromUri(testPath, "../");
+            Assert.True(File.Exists("../SampleCatalog.g.cs"));
+            Assert.Contains("public static List<ContentElement> All = new List<ContentElement>", File.ReadAllText("../SampleCatalog.g.cs"));
         }
     }
 }
