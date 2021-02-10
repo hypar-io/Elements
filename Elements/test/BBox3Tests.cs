b1df4d25494825ca3db8bef33a5527389857dba3
Andrew Heumann <andrew@hypar.io>
2021-02-10 08:54:42 -0500

    add assert

--- Elements/test/BBox3Tests.cs
+++ Elements/test/BBox3Tests.cs
@@ -14,6 +14,8 @@ namespace Elements.Tests
         {
             var polygon = JsonConvert.DeserializeObject<Polygon>("{\"discriminator\":\"Elements.Geometry.Polygon\",\"Vertices\":[{\"X\":-30.52885,\"Y\":-5.09393,\"Z\":0.0},{\"X\":-48.41906,\"Y\":111.94723,\"Z\":0.0},{\"X\":-95.02982,\"Y\":70.39512,\"Z\":0.0},{\"X\":-72.98057,\"Y\":-34.12159,\"Z\":0.0}]}");
             var bbox = new BBox3(polygon);
+            var diagonal = bbox.Max.DistanceTo(bbox.Min);
+            Assert.Equal(159.676157, diagonal, 4);
         }
     }
 }
