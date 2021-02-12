8d38f6617c4dc01b6c2786e1393834d7ab6fe16b
Andrew Heumann <andrew@hypar.io>
2021-02-12 08:55:05 -0500

    Add random color and random material

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -16,6 +16,8 @@
 - `ConvexHull.FromPolylines(IEnumerable<Polyline>)`
 - `ConvexHull.FromProfile(Profile)`
 - `Polygon.FromAlignedBoundingBox2d(IEnumerable<Vector3>)`
+- `Random.NextColor()` and `Random.NextMaterial()`
+
 ### Changed
 - Make MeshConverter deserialization more flexible to accommodate a schema used in function `input_schema`.
 - Prevent the Polygon / Polyline constructors from throwing an exception on duplicate vertices, by removing duplicates automatically.
---
+++ Elements/src/RandomExtensions.cs
@@ -0,0 +1,32 @@
+using System;
+using Elements.Geometry;
+
+namespace Elements
+{
+    /// <summary>
+    /// Extension methods for generating new random objects from an instance of System.Random.
+    /// </summary>
+    public static class RandomExtensions
+    {
+        /// <summary>
+        /// Generate a new color with a random R, G, and B component. Useful for debugging purposes.
+        /// </summary>
+        /// <param name="random"></param>
+        public static Color NextColor(this Random random)
+        {
+            return new Color(random.NextDouble(), random.NextDouble(), random.NextDouble(), 1);
+        }
+
+        /// <summary>
+        /// Generate a new material with a random color assigned. Useful for debugging purposes.
+        /// </summary>
+        /// <param name="random">The `Random` object to generate from</param>
+        /// <param name="unlit">Whether or not to treat the material as unlit.</param>
+        public static Material NextMaterial(this Random random, bool unlit = true)
+        {
+            var color = random.NextColor();
+            return new Material(color, 0.1, 0.3, unlit, null, true, Guid.NewGuid(), color.ToString());
+        }
+    }
+
+}
