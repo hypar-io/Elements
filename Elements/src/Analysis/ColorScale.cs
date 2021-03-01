8922925d9276f3265c434b6d45d47260a9698534
Serena Li <serena@serenali.com>
2021-03-01 14:32:29 -0500

    More explicitly different constructors, tests for banded color scale

--- Elements/src/Analysis/ColorScale.cs
+++ Elements/src/Analysis/ColorScale.cs
@@ -26,6 +26,13 @@ namespace Elements.Analysis
         /// </summary>
         private List<Domain1d> Domains { get; } = null;
 
+        public ColorScale(List<Color> colors)
+        {
+            this.Colors = colors;
+            var domain = new Domain1d(0, 1);
+            this.Domains = domain.DivideByCount(colors.Count - 1).ToList();
+        }
+
         /// <summary>
         /// Construct a color scale.
         /// </summary>
@@ -51,8 +58,6 @@ namespace Elements.Analysis
                 throw new ArgumentException("The color scale could not be created. The number of supplied colors is greater than the final color count.");
             }
 
-            this.Domains = new List<Domain1d>();
-
             var numDomains = (double)(colorCount);
             var colorDomains = new Domain1d(0, 1).DivideByCount(colors.Count - 1).ToList();
 
@@ -70,36 +75,27 @@ namespace Elements.Analysis
                 var tween = (value - foundColorDomain.Min) / foundColorDomain.Length;
                 var color = colors[foundColorDomainIdx].Lerp(colors[foundColorDomainIdx + 1], tween);
                 this.Colors.Add(color);
-                this.Domains.Add(domain);
             }
         }
 
         /// <summary>
-        /// Construct a ColorScale from a list of colors and an optional list of values
+        /// Construct a ColorScale from a list of colors and corresponding values
         /// </summary>
         /// <param name="colors">The color scale's key values.</param>
-        /// <param name="values">List of values each color corresponds to on your scale. If specified, it expects one value per color, in ascending numerical order.</param>
-        public ColorScale(List<Color> colors, List<double> values = null)
+        /// <param name="values">List of values each color corresponds to on your scale. It expects one value per color, in ascending numerical order.</param>
+        public ColorScale(List<Color> colors, List<double> values)
         {
             this.Colors = colors;
 
-            if (values == null)
-            {
-                var domain = new Domain1d(0, 1);
-                this.Domains = domain.DivideByCount(colors.Count - 1).ToList();
-            }
-            else if (colors.Count == values.Count)
-            {
-                this.Domains = new List<Domain1d>();
-
-                for (var i = 0; i < values.Count - 1; i++)
-                {
-                    this.Domains.Add(new Domain1d(values[i], values[i + 1]));
-                }
-            }
-            else
+            if (colors.Count != values.Count)
             {
                 throw new ArgumentException("If you provide a list of custom values, it must match your list of colors in its count of items");
+            }
+            this.Domains = new List<Domain1d>();
+
+            for (var i = 0; i < values.Count - 1; i++)
+            {
+                this.Domains.Add(new Domain1d(values[i], values[i + 1]));
             }
         }
 
@@ -107,33 +103,29 @@ namespace Elements.Analysis
         /// Get the color from the color scale most closely
         /// approximating the provided value.
         /// </summary>
-        /// <param name="t">A number within the numerical parameters from when you constructed your color scale.</param>
+        /// <param name="t">A number within the numerical parameters from when you constructed your color scale. If this was initiated with colorCount, must be between 0 and 1.</param>
         /// <param name="discrete">If true, returns exactly one of the colors initially created, rather than a smoothly interpolated value.</param>
         /// <returns>A color.</returns>
         public Color GetColorForValue(double t)
         {
             if (this.Domains == null)
             {
-                throw new ArgumentException("Your domains have not been calculated. Make sure this was initiated as a linear numerical scale");
+                // This is a discrete value scale from 0 to 1
+                if (t < 0.0 || t > 1.0)
+                {
+                    throw new ArgumentException("The value of t must be between 0.0 and 1.0");
+                }
+                var index = Math.Min((int)Math.Floor(t * (this.Colors.Count)), this.Colors.Count - 1);
+                return this.Colors[index];
             }
-            var domainIdx = GetDomainIndex(t);
 
+            var domainIdx = GetDomainIndex(t);
             if (domainIdx == null)
             {
                 throw new ArgumentException($"Value {t} was not found in color scale");
             }
-
-            // Discrete color scales end up with matching length lists,
-            // non-discrete color scales expect there to be one more color than the number of domains
-            var isDiscrete = this.Colors.Count != this.Domains.Count + 1;
-
             var foundDomainIdx = (int)domainIdx;
             var domain = this.Domains[foundDomainIdx];
-            if (isDiscrete)
-            {
-                var color = t == domain.Max && foundDomainIdx < this.Colors.Count - 1 ? this.Colors[foundDomainIdx + 1] : this.Colors[foundDomainIdx];
-                return color;
-            }
             var colorMin = this.Colors[foundDomainIdx];
             var colorMax = this.Colors[foundDomainIdx + 1];
             var unitizedDistance = (t - domain.Min) / (domain.Max - domain.Min);
--- Elements/test/ColorScaleTests.cs
+++ Elements/test/ColorScaleTests.cs
@@ -29,12 +29,12 @@ namespace Elements.Tests
         }
 
         [Fact]
-        public void GetColor()
+        public void GetInterpolatedColor()
         {
-            var colorScale = new ColorScale(new List<Color>() { Colors.Cyan, Colors.Purple, Colors.Orange });
-            Assert.Equal(colorScale.Colors[1], colorScale.GetColorForValue(0.5));
-            Assert.Equal(colorScale.Colors[0], colorScale.GetColorForValue(0.0));
-            Assert.Equal(colorScale.Colors[2], colorScale.GetColorForValue(1.0));
+            var defaultColorScale = new ColorScale(new List<Color>() { Colors.Cyan, Colors.Purple, Colors.Orange });
+            Assert.Equal(defaultColorScale.Colors[1], defaultColorScale.GetColorForValue(0.5));
+            Assert.Equal(defaultColorScale.Colors[0], defaultColorScale.GetColorForValue(0.0));
+            Assert.Equal(defaultColorScale.Colors[2], defaultColorScale.GetColorForValue(1.0));
 
             var unevenColorScale = new ColorScale(new List<Color>() { Colors.Cyan, Colors.Purple, Colors.Orange }, new List<double>() { 0, 10, 15 });
             Assert.Equal(unevenColorScale.Colors[0], unevenColorScale.GetColorForValue(0.0));
@@ -42,6 +42,24 @@ namespace Elements.Tests
             Assert.Equal(unevenColorScale.Colors[2], unevenColorScale.GetColorForValue(15.0));
 
             Assert.Throws<ArgumentException>(() => unevenColorScale.GetColorForValue(15.1));
+        }
+
+        [Fact]
+        public void GetBandedColor()
+        {
+            var numColors = 10;
+            var colorScale = new ColorScale(new List<Color>() { Colors.Cyan, Colors.Purple, Colors.Orange }, numColors);
+            Assert.Equal(10, colorScale.Colors.Count);
+            for (var i = 0; i < numColors; i++)
+            {
+                Assert.Equal(colorScale.Colors[i], colorScale.GetColorForValue((double)i / (numColors - 1)));
+            }
+        }
+
+        [Fact]
+        public void ThrowsOnSmallerColorCountMismatch()
+        {
+            Assert.Throws<ArgumentException>(() => new ColorScale(new List<Color>() { Colors.Cyan, Colors.Purple }, 1));
         }
 
         [Fact]
