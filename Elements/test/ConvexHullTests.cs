4e64263554caeca8c064e41891ab15ef31d87b43
Serena Li <serena@serenali.com>
2021-04-28 18:24:09 -0400

    Add test

---
+++ Elements/test/ConvexHullTests.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using Elements.Analysis;
+using Elements.Geometry;
+using Xunit;
+
+
+
+namespace Elements.Tests
+{
+    public class ConvexHullTests : ModelTest
+    {
+        [Fact]
+        public void ConvexHullOrthogonal()
+        {
+            var points = new List<Vector3>()
+            {
+                new Vector3(51.8160, 6.0960, 45.7200),
+                new Vector3(0.0000, 6.0960, 45.7200),
+                new Vector3(0.0000, 24.3840, 45.7200),
+                new Vector3(51.8160, 24.3840, 45.7200),
+                new Vector3(51.8160, 24.3840, 45.7200),
+                new Vector3(51.8160, 6.0960, 45.7200)
+            };
+            var hull = ConvexHull.FromPoints(points);
+            Assert.Equal(hull.Segments().Length, 4);
+        }
+    }
+}
