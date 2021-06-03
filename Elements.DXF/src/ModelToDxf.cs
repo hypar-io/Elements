4ef938e692132f30ef7d354de7edae813d02ee13
Eric Wassail <eric@hypar.io>
2021-06-03 09:53:18 -0400

    add xml comments

--- Elements.DXF/src/ModelToDxf.cs
+++ Elements.DXF/src/ModelToDxf.cs
@@ -6,15 +6,25 @@ using Microsoft.Win32.SafeHandles;
 
 namespace Elements.DXF
 {
+    /// <summary>
+    /// This class is capable of rendering a model to DXF using an internal list of dxfCreators
+    /// </summary>
     public class ModelToDxf
     {
-        private Dictionary<Type, IRenderDxf> _dxfCreator = new Dictionary<Type, IRenderDxf>();
+        private Dictionary<Type, IRenderDxf> _dxfCreators = new Dictionary<Type, IRenderDxf>();
 
+        /// <summary>
+        /// Construct a ModelToDxf instance with all of the built in DXFCreators.
+        /// </summary>
         public ModelToDxf()
         {
-            _dxfCreator.Add(typeof(Floor), new FloorToDXF());
+            _dxfCreators.Add(typeof(Floor), new FloorToDXF());
         }
 
+        /// <summary>
+        /// Renders the model in dxf to the returned stream.
+        /// </summary>
+        /// <param name="model">The model to render</param>
         public Stream Render(Model model)
         {
             var doc = new netDxf.DxfDocument(netDxf.Header.DxfVersion.AutoCad2018);
@@ -23,7 +33,7 @@ namespace Elements.DXF
 
             foreach (var element in model.Elements.Values)
             {
-                if (!_dxfCreator.TryGetValue(element.GetType(), out var converter))
+                if (!_dxfCreators.TryGetValue(element.GetType(), out var converter))
                 {
                     continue;
                 }
--- Elements.DXF/src/ToDxfExtensions.cs
+++ Elements.DXF/src/ToDxfExtensions.cs
@@ -5,8 +5,14 @@ using dxf = netDxf.Entities;
 
 namespace Elements.DXF.Extensions
 {
+    /// <summary>
+    /// Extension methods for converting Element geometric primitives into DXF Entities and objects.
+    /// </summary>
     public static class ToDxfExtensions
     {
+        /// <summary>
+        /// Convert to a DXF Polyline entity.
+        /// </summary>
         public static dxf.Polyline ToDxf(this Polyline polyline)
         {
             IEnumerable<netDxf.Vector3> vertices = polyline.Vertices.Select(v => v.ToDxf());
@@ -14,6 +20,9 @@ namespace Elements.DXF.Extensions
             return dxf;
         }
 
+        /// <summary>
+        /// Convert to a DXF Vector3.
+        /// </summary>
         public static netDxf.Vector3 ToDxf(this Vector3 vector3)
         {
             return new netDxf.Vector3(vector3.X, vector3.Y, vector3.Z);
