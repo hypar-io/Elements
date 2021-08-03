991edf78fef6c2e307aa21fa4d1fd13b8d6e1645
Ian Keough <ian@hypar.io>
2021-08-03 16:20:04 -0700

    Set classification moved to file.

--- Elements/src/SetOperations.cs
+++ Elements/src/SetOperations.cs
@@ -6,29 +6,6 @@ using Elements.Spatial;
 namespace Elements
 {
     /// <summary>
-    /// A set containment classification.
-    /// </summary>
-    public enum SetClassification
-    {
-        /// <summary>
-        /// A segments inside B.
-        /// </summary>
-        AInsideB,
-        /// <summary>
-        /// A segments outside B.
-        /// </summary>
-        AOutsideB,
-        /// <summary>
-        /// B segments inside A.
-        /// </summary>
-        BInsideA,
-        /// <summary>
-        /// B segments outside A.
-        /// </summary>
-        BOutsideA
-    }
-
-    /// <summary>
     /// Operations on sets of edges.
     /// </summary>
     public static class SetOperations
