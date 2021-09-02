9cecd9a24062a26d586bea8ed817e043948d7f35
Ian Keough <ian@hypar.io>
2021-09-01 22:17:54 -0700

    Update GetHashCode for Line and Vector3.

--- Elements/src/Geometry/Vector3.cs
+++ Elements/src/Geometry/Vector3.cs
@@ -33,7 +33,14 @@ namespace Elements.Geometry
         /// </summary>
         public override int GetHashCode()
         {
-            return this.ToString().GetHashCode();
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 23 + X.GetHashCode();
+                hash = hash * 23 + Y.GetHashCode();
+                hash = hash * 23 + Z.GetHashCode();
+                return hash;
+            }
         }
 
         /// <summary>
