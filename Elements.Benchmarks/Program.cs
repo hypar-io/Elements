0ec0307fdb64379d850dc8f899862c626a0d8ba9
Ian Keough <ian@hypar.io>
2021-09-07 13:31:59 -0700

    Add a trace project into benchmarks.

--- Elements.Benchmarks/Program.cs
+++ Elements.Benchmarks/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Running;
 using Elements.Geometry;
 using Elements.Geometry.Profiles;
@@ -45,6 +46,20 @@ namespace Elements.Benchmarks
         }
     }
 
+    [EventPipeProfiler(EventPipeProfile.CpuSampling)]
+    [SimpleJob(launchCount: 1, warmupCount: 0, targetCount: 1)]
+    public class Trace
+    {
+        [Benchmark(Description = "Create all HSS beams and serialize to JSON.")]
+        public void TraceModelCreation()
+        {
+            var factory = new HSSPipeProfileFactory();
+            var hssProfiles = factory.AllProfiles().ToList();
+            var model = HSS.DrawAllBeams(hssProfiles);
+            var json = model.ToJson();
+        }
+    }
+
     [MemoryDiagnoser]
     public class HSS
     {
@@ -59,19 +74,17 @@ namespace Elements.Benchmarks
         [Params(true, false)]
         public bool Merge { get; set; }
 
-        public HSS()
+        public static Model DrawAllBeams(List<HSSPipeProfile> profiles)
         {
             var x = 0.0;
             var z = 0.0;
-            var factory = new HSSPipeProfileFactory();
-            _hssProfiles = factory.AllProfiles().ToList();
-            _model = new Model();
-            foreach (var profile in _hssProfiles)
+            var model = new Model();
+            foreach (var profile in profiles)
             {
-                var color = new Color((float)(x / 20.0), (float)(z / _hssProfiles.Count), 0.0f, 1.0f);
+                var color = new Color((float)(x / 20.0), (float)(z / profiles.Count), 0.0f, 1.0f);
                 var line = new Line(new Vector3(x, 0, z), new Vector3(x, 3, z));
                 var beam = new Beam(line, profile);
-                _model.AddElement(beam);
+                model.AddElement(beam);
                 x += 2.0;
                 if (x > 20.0)
                 {
@@ -79,7 +92,15 @@ namespace Elements.Benchmarks
                     x = 0.0;
                 }
             }
+            return model;
+        }
 
+        [GlobalSetup]
+        public void Setup()
+        {
+            var factory = new HSSPipeProfileFactory();
+            _hssProfiles = factory.AllProfiles().ToList();
+            _model = DrawAllBeams(_hssProfiles);
             _json = _model.ToJson();
         }
 
