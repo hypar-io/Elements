18f227209f340b793a4665782ac3bf6d3bd0bdeb
Ian Keough <ian@hypar.io>
2021-10-22 11:14:34 -0700

    Rebuild docs. Update Three.

--- Elements.Playground/wwwroot/js/model.js
+++ Elements.Playground/wwwroot/js/model.js
@@ -1,6 +1,6 @@
-import * as THREE from 'https://unpkg.com/three@0.123.0/build/three.module.js'
-import { OrbitControls } from 'https://unpkg.com/three@0.123.0/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'https://unpkg.com/three@0.123.0/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'https://cdn.skypack.dev/three@0.133.0/build/three.module.js'
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.133.0/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.133.0/examples/jsm/loaders/GLTFLoader.js';
 
 window.model = {
     initialize3D: () => { initialize3D(); },
