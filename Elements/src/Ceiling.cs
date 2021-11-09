48c573b17265771fdd6e0d845c25131a3858392d
katehryhorenko <kate@hypar.io>
2021-11-09 12:54:16 -0800

    Replaced Profile with Perimeter and Elevation;

--- Elements/src/Ceiling.cs
+++ Elements/src/Ceiling.cs
@@ -4,6 +4,7 @@ using Elements.Interfaces;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Elements
 {
@@ -18,9 +19,14 @@ namespace Elements
         public double Thickness { get; protected set; }
 
         /// <summary>
+        /// The elevation of the ceiling.
+        /// </summary>
+        public double Elevation { get; protected set; }
+
+        /// <summary>
         /// The profile of the ceiling.
         /// </summary>
-        public Profile Profile { get; protected set; }
+        public Polygon Perimeter { get; protected set; }
 
         /// <summary>
         /// A collection of openings in the ceiling.
@@ -30,8 +36,10 @@ namespace Elements
         /// <summary>
         /// Construct a ceiling by extruding a profile.
         /// </summary>
-        /// <param name="profile">The plan profile of the ceiling.</param>
+        /// <param name="perimeter">The plan profile of the ceiling. It should lie on XY plane.
+        /// The Z coordinate will be ignored</param>
         /// <param name="thickness">The thickness of the ceiling.</param>
+        /// <param name="elevation">The elevation of the ceiling.</param>
         /// <param name="material">The material of the ceiling.</param>
         /// <param name="transform">An optionional transform for the ceiling.</param>
         /// <param name="representation">The ceiling's representation.</param>
@@ -39,7 +47,50 @@ namespace Elements
         /// <param name="id">The id of the ceiling.</param>
         /// <param name="name">The name of the ceiling.</param>
         [JsonConstructor]
-        public Ceiling(Profile profile,
+        public Ceiling(Polygon perimeter,
+                      double thickness,
+                      double elevation,
+                      Material material = null,
+                      Transform transform = null,
+                      Representation representation = null,
+                      bool isElementDefinition = false,
+                      Guid id = default(Guid),
+                      string name = null) : base(transform != null ? transform : new Transform(),
+                                                 material != null ? material : BuiltInMaterials.Concrete,
+                                                 representation != null ? representation : new Representation(new List<SolidOperation>()),
+                                                 isElementDefinition,
+                                                 id != default(Guid) ? id : Guid.NewGuid(),
+                                                 name)
+        {
+            if (thickness <= 0.0)
+            {
+                throw new ArgumentOutOfRangeException("The ceiling could not be created. The thickness of the ceiling must be greater than 0.0.");
+            }
+
+            if (!perimeter.Normal().IsParallelTo(Vector3.ZAxis))
+            {
+                throw new ArgumentOutOfRangeException("The ceiling could not be created. The perimeter polygon must lie on XY plane");
+            }
+
+            this.Elevation = elevation;
+            this.Perimeter = perimeter.Project(new Plane(Vector3.Origin, Vector3.ZAxis)); ;
+            this.Thickness = thickness;
+            Transform.Move(Vector3.ZAxis * Elevation);
+        }
+
+        /// <summary>
+        /// Construct a ceiling by extruding a profile.
+        /// </summary>
+        /// <param name="perimeter">The plan perimeter of the ceiling. It should lie on XY plane.
+        /// Z coordinate will be used as elevation</param>
+        /// <param name="thickness">The thickness of the ceiling.</param>
+        /// <param name="material">The material of the ceiling.</param>
+        /// <param name="transform">An optionional transform for the ceiling.</param>
+        /// <param name="representation">The ceiling's representation.</param>
+        /// <param name="isElementDefinition">Is this an element definition?</param>
+        /// <param name="id">The id of the ceiling.</param>
+        /// <param name="name">The name of the ceiling.</param>
+        public Ceiling(Polygon perimeter,
                       double thickness,
                       Material material = null,
                       Transform transform = null,
@@ -58,8 +109,43 @@ namespace Elements
                 throw new ArgumentOutOfRangeException("The ceiling could not be created. The thickness of the ceiling must be greater than 0.0.");
             }
 
-            this.Profile = profile;
+            if (!perimeter.Normal().IsParallelTo(Vector3.ZAxis))
+            {
+                throw new ArgumentOutOfRangeException("The ceiling could not be created. The perimeter polygon must lie on XY plane");
+            }
+
+            // we do not need null check cause id there is no vertices it will fail on calculating Normal
+            this.Elevation = perimeter.Vertices.First().Z;
+            this.Perimeter = perimeter.Project(new Plane(Vector3.Origin, Vector3.ZAxis));
             this.Thickness = thickness;
+            Transform.Move(Vector3.ZAxis * Elevation);
+        }
+
+        /// <summary>
+        /// The Profile of the Ceiling computed from its Perimeter and the Openings.
+        /// </summary>
+        /// <returns></returns>
+        public Profile GetProfile()
+        {
+            return new Profile(Perimeter, Openings?.Select(o => o.Perimeter).ToList());
+        }
+
+        /// <summary>
+        /// Add an Opening to the Ceiling.
+        /// </summary>
+        /// <param name="perimeter">The plan perimeter of the ceiling. It should lie on XY plane.
+        /// The Z coordinate will be ignored</param>
+        /// <param name="depthFront"></param>
+        /// <param name="depthBack"></param>
+        public void AddOpening(Polygon perimeter, double depthFront = 1, double depthBack = 1)
+        {
+            if (!perimeter.Normal().IsParallelTo(Vector3.ZAxis))
+            {
+                throw new ArgumentOutOfRangeException("The opening could not be created. The perimeter polygon must lie on XY plane");
+            }
+
+            var opening = new Opening(perimeter.Project(new Plane(Vector3.Origin, Vector3.ZAxis)), perimeter.Normal(), depthFront, depthBack);
+            this.Openings.Add(opening);
         }
 
         /// <summary>
@@ -68,7 +154,8 @@ namespace Elements
         public override void UpdateRepresentations()
         {
             this.Representation.SolidOperations.Clear();
-            this.Representation.SolidOperations.Add(new Extrude(this.Profile, this.Thickness, Vector3.ZAxis, false));
+            var extrude = new Extrude(GetProfile(), this.Thickness, Vector3.ZAxis, false);
+            this.Representation.SolidOperations.Add(extrude);
         }
 
         /// <summary>
