2f8af708afd6b3504f5ff17d0ae73eb6b3879b67
Andrew Heumann <andrew@hypar.io>
2021-12-08 07:00:58 -0500

    use ContentCatalog.FromJson

--- Elements.Components/src/ContentCatalogRetrieval.cs
+++ Elements.Components/src/ContentCatalogRetrieval.cs
@@ -41,16 +41,7 @@ namespace Elements.Components
                     throw new Exception("Catalog not found. To use this class, make sure you have a content catalog stored at catalog.json in the build directory.");
                 }
                 var json = File.ReadAllText(catalogPath);
-                // there are two catalog formats in the wild — one encoded as a model, one with the content catalog encoded bare.
-                try
-                {
-                    var model = Model.FromJson(json);
-                    catalog = model.AllElementsOfType<ContentCatalog>().First();
-                }
-                catch
-                {
-                    catalog = JsonConvert.DeserializeObject<ContentCatalog>(json);
-                }
+                catalog = ContentCatalog.FromJson(json);
                 // Mutate IDs, otherwise we run into a bug with the export server, collisions with shared catalogs, big messy mess.
                 foreach (var element in catalog.Content)
                 {
