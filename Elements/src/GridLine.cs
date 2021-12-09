e36dfc87a955cacd7a074642bfb0eb92b915a39d
Serena Li <serena@serenali.com>
2021-12-09 10:11:26 -0800

    Support curved gridlines

--- Elements/src/GridLine.cs
+++ Elements/src/GridLine.cs
@@ -13,7 +13,14 @@ namespace Elements
         /// Line that runs from the start of the gridline to its end.
         /// </summary>
         /// <value></value>
+        [Obsolete("We now use 'Curve' instead.")]
         public Line Line { get; set; }
+
+        /// <summary>
+        /// Curve that runs from the start of the gridline to its end.
+        /// </summary>
+        /// <value></value>
+        public Curve Curve { get; set; }
 
         /// <summary>
         /// Radius of the gridline head.
@@ -32,7 +39,7 @@ namespace Elements
 
         internal override Boolean TryToGraphicsBuffers(out List<GraphicsBuffers> graphicsBuffers, out string id, out glTFLoader.Schema.MeshPrimitive.ModeEnum? mode)
         {
-            if (this.Line == null)
+            if (this.Curve == null)
             {
                 return base.TryToGraphicsBuffers(out graphicsBuffers, out id, out mode);
             }
@@ -41,25 +48,45 @@ namespace Elements
             mode = glTFLoader.Schema.MeshPrimitive.ModeEnum.LINES;
             graphicsBuffers = new List<GraphicsBuffers>();
 
-            var line = new ModelCurve(this.Line);
-            graphicsBuffers.Add(line.ToGraphicsBuffers(true));
+            var curve = new ModelCurve(this.Curve);
+            graphicsBuffers.Add(curve.ToGraphicsBuffers(true));
 
-            var dir = this.Line.Direction();
+            var start = this.Curve.PointAt(0);
+            var end = this.Curve.PointAt(1);
+
+            var dirStart = this.Curve.TransformAt(0).ZAxis;
+            var dirEnd = this.Curve.TransformAt(1).ZAxis;
+
+            // TransformAt does not seem to consistently point either from start to end, or end to start,
+            // so we normalize for that here.
+            // TODO: fix TransformAt for all curves so that they point in a consistent direction?
+
+            var segments = this.Curve.ToPolyline().Segments();
+            var firstSegment = segments[0];
+            if (firstSegment.Direction().PlaneAngleTo(dirStart) > Math.PI / 2)
+            {
+                dirStart = dirStart.Negate();
+            }
+            var lastSegment = segments[segments.Length - 1];
+            if (lastSegment.Direction().PlaneAngleTo(dirEnd) > Math.PI / 2)
+            {
+                dirEnd = dirEnd.Negate();
+            }
 
             if (ExtensionBeginning > 0)
             {
-                var extensionBeginning = new ModelCurve(new Line(this.Line.Start, this.Line.Start - dir * ExtensionBeginning));
+                var extensionBeginning = new ModelCurve(new Line(start, start - dirStart * ExtensionBeginning));
                 graphicsBuffers.Add(extensionBeginning.ToGraphicsBuffers(true));
             }
 
             if (ExtensionEnd > 0)
             {
-                var extensionEnd = new ModelCurve(new Line(this.Line.End, this.Line.End + dir * ExtensionEnd));
+                var extensionEnd = new ModelCurve(new Line(end, end + dirEnd * ExtensionEnd));
                 graphicsBuffers.Add(extensionEnd.ToGraphicsBuffers(true));
             }
 
             var circle = new Circle(Radius);
-            circle.Center = this.Line.Start - dir * (ExtensionBeginning + Radius);
+            circle.Center = start - dirStart * (ExtensionBeginning + Radius);
             graphicsBuffers.Add(new ModelCurve(circle).ToGraphicsBuffers(true));
             return true;
         }
--- Elements/test/GridLineTests.cs
+++ Elements/test/GridLineTests.cs
@@ -19,8 +19,25 @@ namespace Elements.Tests.Examples
 
             var gridline = new GridLine();
             gridline.Name = "A";
-            gridline.Line = new Line(new Vector3(), new Vector3(25, 25, 0));
+            gridline.Curve = new Line(new Vector3(), new Vector3(25, 25, 0));
             gridline.Material = new Material("Red", new Color(1, 0, 0, 1));
+
+            this.Model.AddElement(gridline);
+        }
+
+        [Fact]
+        public void CurvedGridline()
+        {
+            this.Name = "Elements_GridLinesCurved";
+
+            var size = 50;
+
+            var gridline = new GridLine();
+            gridline.Name = "A";
+            gridline.Material = new Material("Red", new Color(1, 0, 0, 1));
+            gridline.Curve = new Arc(new Vector3(), size, 0, 180);
+            gridline.ExtensionBeginning = 100;
+            gridline.ExtensionEnd = 100;
 
             this.Model.AddElement(gridline);
         }
