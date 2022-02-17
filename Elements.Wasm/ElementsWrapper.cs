ff4a309c44af470b2bf37e9c760d5f1f6c91f7ab
Ian Keough <ian@hypar.io>
2022-02-17 11:06:15 -0800

    Disable validation on construction.

--- Elements.Wasm/ElementsWrapper.cs
+++ Elements.Wasm/ElementsWrapper.cs
@@ -4,6 +4,7 @@ using Elements;
 using Elements.Geometry;
 using Elements.Serialization.glTF;
 using Elements.Spatial;
+using Elements.Validators;
 using Microsoft.JSInterop;
 using System.Diagnostics;
 
@@ -18,6 +19,8 @@ public static class ElementsAPI
     [JSInvokable]
     public static Task<string> ModelToGlbBase64(string json)
     {
+        Validator.DisableValidationOnConstruction = true;
+
         var model = Model.FromJson(json, out _, false);
         return Task.FromResult(model.ToBase64String());
     }
@@ -25,10 +28,16 @@ public static class ElementsAPI
     [JSInvokable]
     public static Task<byte[]> ModelToGlbBytes(string json)
     {
+        Validator.DisableValidationOnConstruction = true;
+
         var sw = new Stopwatch();
         sw.Start();
-        var model = Model.FromJson(json, out _, false);
+        var model = Model.FromJson(json, out var errors, false);
         Console.WriteLine($"{sw.ElapsedMilliseconds}ms for creating the model from json.");
+        foreach (var error in errors)
+        {
+            Console.WriteLine(error);
+        }
         sw.Restart();
         var result = Task.FromResult(model.ToGlTF());
         Console.WriteLine($"{sw.ElapsedMilliseconds}ms for creating the glb from json.");
