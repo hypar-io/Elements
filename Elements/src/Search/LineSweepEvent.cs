5d3f99b262bac8f2c87b6981ce05f0a20e92900b
Ian Keough <ian@hypar.io>
2022-03-18 10:26:58 -0700

    Don't implement comparable in favor of doing ordering in calling code.

--- Elements/src/Search/LineSweepEvent.cs
+++ Elements/src/Search/LineSweepEvent.cs
@@ -4,7 +4,7 @@ using Elements.Geometry;
 
 namespace Elements.Search
 {
-    internal class LineSweepEvent<T> : IComparable<LineSweepEvent<T>>
+    internal class LineSweepEvent<T>
     {
         public Vector3 Point;
 
@@ -14,19 +14,6 @@ namespace Elements.Search
         {
             this.Point = point;
             this.Segments = segments;
-        }
-
-        public int CompareTo(LineSweepEvent<T> other)
-        {
-            if (this.Point.X < other.Point.X)
-            {
-                return -1;
-            }
-            else if (this.Point.X > other.Point.X)
-            {
-                return 1;
-            }
-            return 0;
         }
 
         public override string ToString()
