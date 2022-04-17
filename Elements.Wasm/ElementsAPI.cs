27afc0f15e255f2d3bb99030cd70c85be2db8db1
Ian Keough <ian@hypar.io>
2022-04-17 00:29:36 -0700

    Update the bridge API.

--- Elements.Wasm/ElementsAPI.cs
+++ Elements.Wasm/ElementsAPI.cs
@@ -14,7 +14,7 @@ public static class ElementsAPI
     [JSInvokable]
     public static Task<Model> ModelFromJson(string json)
     {
-        return Task.FromResult(Model.FromJsonNew(json));
+        return Task.FromResult(Model.FromJson(json));
     }
 
     [JSInvokable]
@@ -22,7 +22,7 @@ public static class ElementsAPI
     {
         Validator.DisableValidationOnConstruction = true;
 
-        var model = Model.FromJsonNew(json);
+        var model = Model.FromJson(json);
         return Task.FromResult(model.ToBase64String());
     }
 
@@ -52,11 +52,11 @@ public static class ElementsAPI
         sb.AppendLine($"{sw.ElapsedMilliseconds}ms for creating test beams.");
         sw.Restart();
 
-        var json = model.ToJsonNew();
+        var json = model.ToJson();
         sb.AppendLine($"{sw.ElapsedMilliseconds}ms for serializing model.");
         sw.Restart();
 
-        var newModel = Model.FromJsonNew(json);
+        var newModel = Model.FromJson(json);
         sb.AppendLine($"{sw.ElapsedMilliseconds}ms for deserializing model.");
         sw.Restart();
 
@@ -82,7 +82,7 @@ public static class ElementsAPI
 
         var sw = new Stopwatch();
         sw.Start();
-        var model = Model.FromJsonNew(json);
+        var model = Model.FromJson(json);
         Console.WriteLine($"{sw.ElapsedMilliseconds}ms for creating the model from json.");
         sw.Restart();
         var result = Task.FromResult(model.ToGlTF());
