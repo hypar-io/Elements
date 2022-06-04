d882e82363c4faa7ba38eec505734c05b3cadbb2
Ian Keough <ian@hypar.io>
2022-06-04 15:52:00 -0700

    Rename annotation in docs.

--- Elements/src/Annotations/Dimension.cs
+++ Elements/src/Annotations/Dimension.cs
@@ -3,32 +3,32 @@ using Elements.Geometry;
 namespace Elements.Annotations
 {
     /// <summary>
-    /// A dimension.
+    /// An annotation.
     /// </summary>
     public abstract class Annotation : Element
     {
         /// <summary>
-        /// The plane in which the dimension is drawn.
+        /// The plane in which the annotation is drawn.
         /// </summary>
         public Plane Plane { get; protected set; } = new Plane(Vector3.Origin, Vector3.ZAxis);
 
         /// <summary>
-        /// Text to be displayed in place of the dimension's value.
+        /// Text to be displayed in place of the annotation's value.
         /// </summary>
         public string DisplayValue { get; set; }
 
         /// <summary>
-        /// Text to be displayed before the dimension's value.
+        /// Text to be displayed before the annotation's value.
         /// </summary>
         public string Prefix { get; set; }
 
         /// <summary>
-        /// Text to be displayed after the dimension's value.
+        /// Text to be displayed after the annotation's value.
         /// </summary>
         public string Suffix { get; set; }
 
         /// <summary>
-        /// Create a dimension.
+        /// Create a annotation.
         /// </summary>
         public Annotation() { }
     }
