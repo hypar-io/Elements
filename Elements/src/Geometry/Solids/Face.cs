bed7c3895f847f2763dd7855ad194fecedab78bc
Ian Keough <ian@hypar.io>
2022-06-19 16:26:56 -0700

    Back out a couple of unnecessary changes.

--- Elements/src/Geometry/Solids/Face.cs
+++ Elements/src/Geometry/Solids/Face.cs
@@ -1,7 +1,4 @@
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using Elements.Search;
 
 namespace Elements.Geometry.Solids
 {
@@ -57,65 +54,6 @@ namespace Elements.Geometry.Solids
                 sb.AppendLine($"HalfEdge: {he.ToString()}");
             }
             return sb.ToString();
-        }
-
-
-        /// <summary>
-        /// Does this face intersect with the provided plane?
-        /// </summary>
-        /// <param name="plane">The intersection plane.</param>
-        /// <param name="lines">Line segments created by the intersection.</param>
-        /// <returns>True if the plane intersects the face, otherwise false.</returns>
-        public bool Intersects(Plane plane, out List<Line> lines)
-        {
-            var pts = new List<Vector3>();
-
-            var outerVerts = this.Outer.Edges.Select(e => e.Vertex).ToList();
-            for (var i = 0; i < outerVerts.Count; i++)
-            {
-                var a = outerVerts[i].Point;
-                var b = i == outerVerts.Count - 1 ? outerVerts[0].Point : outerVerts[i + 1].Point;
-                var l = new Line(a, b);
-                if ((a, b).Intersects(plane, out var result))
-                {
-                    pts.Add(result);
-                }
-            }
-
-            if (this.Inner != null)
-            {
-                foreach (var inner in Inner)
-                {
-                    var innerVerts = inner.Edges.Select(e => e.Vertex).ToList();
-                    for (var i = 0; i < innerVerts.Count; i++)
-                    {
-                        var a = innerVerts[i].Point;
-                        var b = i == innerVerts.Count - 1 ? innerVerts[0].Point : innerVerts[i + 1].Point;
-                        var l = new Line(a, b);
-                        if ((a, b).Intersects(plane, out var result))
-                        {
-                            pts.Add(result);
-                        }
-                    }
-                }
-            }
-
-            if (pts.Count < 2)
-            {
-                lines = null;
-                return false;
-            }
-
-            // var start = pts[0];
-            // pts.Sort(new DistanceComparer(start));
-            var d = this.Plane().Normal.Cross(plane.Normal).Unitized();
-            pts.Sort(new DirectionComparer(d));
-            lines = new List<Line>();
-            for (var i = 0; i < pts.Count; i += 2)
-            {
-                lines.Add(new Line(pts[i], pts[i + 1]));
-            }
-            return true;
         }
     }
 }
