3cd35c1c7562f511518c4d16b15b22d9cff90772
Ian Keough <ian@hypar.io>
2022-07-13 15:00:36 -0700

    Invalidate after deploy.

--- Elements.Wasm/deploy.sh
+++ Elements.Wasm/deploy.sh
@@ -1,47 +1,26 @@
 #!/usr/bin/env bash
 
-configuration='debug'
+source='./bin/Release/net6.0/wwwroot/_framework'
 
-while getopts c: flag
-do
-    case "${flag}" in
-        c) configuration=${OPTARG};;
-    esac
-done
+echo "Building the app..."
+dotnet publish -c release
 
-if [ $configuration == 'release' ]
-then
-    source='./bin/Release/net6.0/wwwroot/_framework'
-elif [ $configuration == 'debug'  ]
-then
-    source='./bin/Debug/net6.0/wwwroot/_framework'
-else
-    echo "The configuration, $configuration, is invalid. Only 'release' and 'debug' are supported."
-    exit
-fi
-
-# echo "Building the app in $configuration configuration."
-# if [ $configuration == 'release' ]
-# then
-#     dotnet publish -c release
-# elif [ $configuration == 'debug'  ]
-# then
-#     dotnet build -c release
-# fi
-
-echo 'Echo creating deploy directory.'
+echo 'Creating deploy directory...'
 rm -r deploy
 mkdir deploy
 mkdir deploy/elements
 mkdir deploy/elements/_framework
 
-echo "Copying assets from $source"
+echo "Copying assets from $source..."
 rsync -av $source deploy/elements
 cp ./wwwroot/elements.js deploy/elements/elements.js 
 cp ./wwwroot/index.html deploy/index.html
 
-echo "Uploading assets to s3"
+echo "Uploading assets to s3..."
 aws s3 sync "$source" s3://elements-wasm/ --exclude "*.gz" --exclude "*.br"
+
+echo "Creating an invalidation..."
+aws cloudfront create-invalidation --distribution-id E19YQ16H2KTDNU --paths "/*"
 
 # echo 'Running the test application.'
 # cd deploy
