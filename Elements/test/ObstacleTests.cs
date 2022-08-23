46552a1adfb7266de607deb263b841eba592f142
mwojcik-sagiton <97615778+mwojcik-sagiton@users.noreply.github.com>
2022-08-23 14:04:15 +0200

    Update ObstacleTests.cs

--- Elements/test/ObstacleTests.cs
+++ Elements/test/ObstacleTests.cs
@@ -1,16 +1,20 @@
 ﻿using Elements.Geometry;
+using Elements.Serialization.glTF;
 using Elements.Spatial.AdaptiveGrid;
+using Elements.Tests;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace Elements
 {
-    public class ObstacleTests 
+
+    public class ObstacleTests : ModelTest
     {
 
         [Theory]
         [MemberData(nameof(GetIntersectsData))]
-        public void IntersectsTest(Polyline polyline, bool expectedResult)
+        public void IntersectsTestDefaultConstructor(Polyline polyline, bool expectedResult, int testNumber)
         {
             var rectangle = Polygon.Rectangle(10, 10);
             var obstacle = new Obstacle(rectangle, 10, 0, false, false, null);
@@ -20,30 +24,62 @@ namespace Elements
             Assert.Equal(expectedResult, result);
         }
 
+        [Theory]
+        [MemberData(nameof(GetIntersectsData))]
+        public void IntersectsTestDefaultConstructorWithOffsetAndTransform(Polyline polyline, bool expectedResult, int testNumber)
+        {
+            var rectangle = Polygon.Rectangle(8, 8);
+            var transform = new Transform(0, 0, 1);
+            var obstacle = new Obstacle(rectangle, 8, 1, false, false, transform);
+
+            var result = obstacle.Intersects(polyline);
+
+            Model.AddElement(obstacle);
+            Model.AddElements(polyline.Segments().Select(x => new ModelCurve(x, BuiltInMaterials.XAxis)));
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetIntersectsData))]
+        public void IntersectsTestFromBBox(Polyline polyline, bool expectedResult, int testNumber)
+        {
+            var bbox = new BBox3(new Vector3(-4, -4, 1), new Vector3(4, 4, 9));
+            var obstacle = Obstacle.FromBBox(bbox, 1);
+
+            var result = obstacle.Intersects(polyline);
+
+            Model.AddElement(obstacle);
+            Model.AddElements(polyline.Segments().Select(x => new ModelCurve(x, BuiltInMaterials.XAxis)));
+            Model.ToGlTF($"../../../Test{testNumber}.gltf");
+
+            Assert.Equal(expectedResult, result);
+        }
+
         public static IEnumerable<object[]> GetIntersectsData()
         {
             var smallPolygon = Polygon.Rectangle(5, 5);
             //Polygon fully inside
-            yield return new object[] { smallPolygon.TransformedPolygon(new Transform(0, 0, 2)), true};
+            yield return new object[] { smallPolygon.TransformedPolygon(new Transform(0, 0, 2)), true, 1 };
             //Polygon fully outside below 
-            yield return new object[] { smallPolygon.TransformedPolygon(new Transform(0, 0, -2)), false};
+            yield return new object[] { smallPolygon.TransformedPolygon(new Transform(0, 0, -2)), false, 2 };
             //Polygon fully outside on side 
-            yield return new object[] { smallPolygon.TransformedPolygon(new Transform().Rotated(Vector3.YAxis, 90).Moved(7, 0, 5)), false};
+            yield return new object[] { smallPolygon.TransformedPolygon(new Transform().Rotated(Vector3.YAxis, 90).Moved(7, 0, 5)), false, 3 };
             //Only one vertex inside
-            yield return new object[] { smallPolygon.TransformedPolygon(new Transform(5, 5, 2)), true};
+            yield return new object[] { smallPolygon.TransformedPolygon(new Transform(5, 5, 2)), true, 4 };
             //Vertex on perimeter
-            yield return new object[] { smallPolygon.TransformedPolygon(new Transform(10, 10, 2)), false};
+            yield return new object[] { smallPolygon.TransformedPolygon(new Transform(10, 10, 2)), false, 5 };
 
             var bigPolygon = Polygon.Rectangle(20, 20);
             //Obstacle inside polygon 
-            yield return new object[] { bigPolygon.TransformedPolygon(new Transform(0, 0, 2)), false};
+            yield return new object[] { bigPolygon.TransformedPolygon(new Transform(0, 0, 2)), false, 6 };
             //One segment intersecting with obstacle
-            yield return new object[] { bigPolygon.TransformedPolygon(new Transform(10, 0, 2)), true};
+            yield return new object[] { bigPolygon.TransformedPolygon(new Transform(10, 0, 2)), true, 7 };
 
             //Polyline on bottom plane of obstacle
-            yield return new object[] { new Polyline(new Vector3(-10, 0), new Vector3(10, 0)), true};
+            yield return new object[] { new Polyline(new Vector3(-10, 0), new Vector3(10, 0)), true, 8 };
             //Polyline on top plane of obstacle
-            yield return new object[] { new Polyline(new Vector3(-10, 0, 10), new Vector3(10, 0, 10)), true};
+            yield return new object[] { new Polyline(new Vector3(-10, 0, 10), new Vector3(10, 0, 10)), true, 9 };
             //Polyline on bottom plane of obstacle, but not intersecting
             yield return new object[] { new Polyline
             (
@@ -51,7 +87,7 @@ namespace Elements
                 new Vector3(-10, 10),
                 new Vector3(10, 10),
                 new Vector3(10, 0)
-            ), false};
+            ), false, 10};
             //Polyline on bottom plane of obstacle, but not intersecting
             yield return new object[] { new Polyline
             (
@@ -59,7 +95,7 @@ namespace Elements
                 new Vector3(0, 0, -2),
                 new Vector3(0, 0, 12),
                 new Vector3(10, 0, 12)
-            ), true};
+            ), true, 11};
             //Polyline on bottom plane of obstacle, but not intersecting
             yield return new object[] { new Polyline
             (
@@ -67,7 +103,7 @@ namespace Elements
                 new Vector3(-6, 0, -2),
                 new Vector3(-6, 0, 12),
                 new Vector3(10, 0, 12)
-            ), false};
+            ), false, 12};
         }
 
         [Fact]
