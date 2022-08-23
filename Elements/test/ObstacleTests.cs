8a2dac2d628a578499840e3b071a042f835443d4
mwojcik-sagiton <97615778+mwojcik-sagiton@users.noreply.github.com>
2022-08-23 13:00:32 +0200

    Update unit tests

--- Elements/test/ObstacleTests.cs
+++ Elements/test/ObstacleTests.cs
@@ -13,7 +13,7 @@ namespace Elements
         public void IntersectsTest(Polyline polyline, bool expectedResult)
         {
             var rectangle = Polygon.Rectangle(10, 10);
-            var obstacle = Obstacle.From2DPolygon(rectangle, 10);
+            var obstacle = new Obstacle(rectangle, 10, 0, false, false, null);
 
             var result = obstacle.Intersects(polyline);
 
@@ -24,92 +24,62 @@ namespace Elements
         {
             var smallPolygon = Polygon.Rectangle(5, 5);
             //Polygon fully inside
-            yield return new object[] { smallPolygon.TransformedPolygon(new Transform(0, 0, 2)), true };
-            //Polygon fully outside
-            yield return new object[] { smallPolygon.TransformedPolygon(new Transform(0, 0, -2)), false };
+            yield return new object[] { smallPolygon.TransformedPolygon(new Transform(0, 0, 2)), true};
+            //Polygon fully outside below 
+            yield return new object[] { smallPolygon.TransformedPolygon(new Transform(0, 0, -2)), false};
+            //Polygon fully outside on side 
+            yield return new object[] { smallPolygon.TransformedPolygon(new Transform().Rotated(Vector3.YAxis, 90).Moved(7, 0, 5)), false};
             //Only one vertex inside
-            yield return new object[] { smallPolygon.TransformedPolygon(new Transform(5, 5, 2)), true };
+            yield return new object[] { smallPolygon.TransformedPolygon(new Transform(5, 5, 2)), true};
             //Vertex on perimeter
-            yield return new object[] { smallPolygon.TransformedPolygon(new Transform(10, 10, 2)), false };
+            yield return new object[] { smallPolygon.TransformedPolygon(new Transform(10, 10, 2)), false};
 
             var bigPolygon = Polygon.Rectangle(20, 20);
             //Obstacle inside polygon 
-            yield return new object[] { bigPolygon.TransformedPolygon(new Transform(0, 0, 2)), false };
+            yield return new object[] { bigPolygon.TransformedPolygon(new Transform(0, 0, 2)), false};
             //One segment intersecting with obstacle
-            yield return new object[] { bigPolygon.TransformedPolygon(new Transform(10, 0, 2)), true };
+            yield return new object[] { bigPolygon.TransformedPolygon(new Transform(10, 0, 2)), true};
 
             //Polyline on bottom plane of obstacle
-            yield return new object[] { new Polyline(new Vector3(-10, 0), new Vector3(10, 0)), true };
+            yield return new object[] { new Polyline(new Vector3(-10, 0), new Vector3(10, 0)), true};
             //Polyline on top plane of obstacle
-            yield return new object[] { new Polyline(new Vector3(-10, 0, 10), new Vector3(10, 0, 10)), true };
-            //Polyline on top plane of obstacle
+            yield return new object[] { new Polyline(new Vector3(-10, 0, 10), new Vector3(10, 0, 10)), true};
+            //Polyline on bottom plane of obstacle, but not intersecting
             yield return new object[] { new Polyline
             (
-                new Vector3(-10, 0), 
-                new Vector3(-10, 10), 
-                new Vector3(10, 10), 
+                new Vector3(-10, 0),
+                new Vector3(-10, 10),
+                new Vector3(10, 10),
                 new Vector3(10, 0)
-            ), false };
+            ), false};
+            //Polyline on bottom plane of obstacle, but not intersecting
+            yield return new object[] { new Polyline
+            (
+                new Vector3(-10, 0, -2),
+                new Vector3(0, 0, -2),
+                new Vector3(0, 0, 12),
+                new Vector3(10, 0, 12)
+            ), true};
+            //Polyline on bottom plane of obstacle, but not intersecting
+            yield return new object[] { new Polyline
+            (
+                new Vector3(-10, 0, -2),
+                new Vector3(-6, 0, -2),
+                new Vector3(-6, 0, 12),
+                new Vector3(10, 0, 12)
+            ), false};
         }
 
         [Fact]
         public void IntersectsRotatedPolygon()
         {
             var polygon = new Polygon(Vector3.Origin, new Vector3(5, 5), new Vector3(0, 10), new Vector3(-5, 5));
-            var obstacle = Obstacle.From2DPolygon(polygon, 5);
+            var obstacle = new Obstacle(polygon, 5, 0, false, false, null);
             var polyline = new Polyline(new Vector3(-4, 6), new Vector3(-1, 9));
 
             var result = obstacle.Intersects(polyline);
 
             Assert.False(result);
-        }
-
-        [Fact]
-        public void IsInsideTests()
-        {
-            var polygon = new Polygon(Vector3.Origin, new Vector3(5, 5), new Vector3(0, 10), new Vector3(-5, 5));
-            var obstacle = Obstacle.From2DPolygon(polygon, 5);
-
-            var vertexPoint = Vector3.Origin;
-            Assert.False(obstacle.IsInside(vertexPoint));
-
-            var verticalEdgePoint = new Vector3(0, 0, 3);
-            Assert.False(obstacle.IsInside(verticalEdgePoint));
-
-            var horizontalEdgePoint = new Vector3(-2, 2);
-            Assert.False(obstacle.IsInside(horizontalEdgePoint));
-
-            var bottomMiddlePoint = new Vector3(0, 5);
-            Assert.True(obstacle.IsInside(bottomMiddlePoint));
-
-            var pointBelow = new Vector3(0, 5, -2);
-            Assert.False(obstacle.IsInside(pointBelow));
-
-            var topMiddlePoint = new Vector3(0, 5, 5);
-            Assert.True(obstacle.IsInside(topMiddlePoint));
-
-            var pointAbove = new Vector3(0, 5, 7);
-            Assert.False(obstacle.IsInside(pointAbove));
-
-            var pointOnFace = new Vector3(2, 2, 2);
-            Assert.False(obstacle.IsInside(pointOnFace));
-
-            var pointInside = new Vector3(0, 2, 2);
-            Assert.True(obstacle.IsInside(pointInside));
-
-            var pointOutside = new Vector3(2, 0, 2);
-            Assert.False(obstacle.IsInside(pointOutside));
-
-            var flatObstacle = Obstacle.FromBBox(new BBox3(new Vector3(2, 2), new Vector3(4, 4)), perimeter: true);
-            
-            vertexPoint = new Vector3(2, 2);
-            Assert.False(flatObstacle.IsInside(vertexPoint));
-
-            var edgePoint = new Vector3(3, 4);
-            Assert.False(flatObstacle.IsInside(edgePoint));
-
-            pointInside = new Vector3(3, 3);
-            Assert.True(flatObstacle.IsInside(pointInside));
         }
     }
 }
