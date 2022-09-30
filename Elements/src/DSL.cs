6f385a1143134e25213f7fb812c99f0c59a82f8e
Ian Keough <ian@iankeough.com>
2022-09-30 06:42:29 -0500

    Initial commit.

---
+++ Elements/src/DSL.cs
@@ -0,0 +1,10 @@
+namespace Elements.DSL
+{
+    public static class ElementDSL
+    {
+        public static CreatePointsAlongACurveWithEvenSpacing(Curve curve, int numberOfPoints)
+        {
+            
+        }
+    }
+}
