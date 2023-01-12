a139eaed95c1efb2f2e7e2f72f18b2263f181153
Artur Kravtsov <akravtsov@amcbridge.com>
2023-01-12 09:48:51 +0200

    added comments to the max heap class

--- Elements/src/Algorithms/BinaryHeap.cs
+++ Elements/src/Algorithms/BinaryHeap.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Elements.Algorithms
 {
+    // See
+    // https://en.wikipedia.org/wiki/Binary_heap
+    // for a detailed explanation of the operations
     class BinaryHeap<TKey, TValue> where TKey : IComparable<TKey>
     {
         public BinaryHeap()
@@ -14,6 +17,9 @@ namespace Elements.Algorithms
 
         private void heapify(int i)
         {
+            // corrects a heap, rooted in the ith vertex, assuming the children are already roots of valid heaps
+            // it swaps the root with the maximal of its children, if necessary, and calls recursively on that child
+            // tail-recursion is expanded into a loop
             while (true)
             {
                 int j = (i << 1) + 1;
@@ -27,6 +33,10 @@ namespace Elements.Algorithms
 
         public BinaryHeap((TKey, TValue)[] array)
         {
+            // construct a binary heap from the given array of numbers
+            // Asymptotic complexity: O(n), where n is the size of the array
+            // 
+            // Algorithm: makes the subtrees valid heaps starting from the bottom up
             n = array.Length;
             a = new List<(TKey, TValue)>(array);
             for (int i = n - 1; i >= 0; --i) heapify(i);
@@ -34,6 +44,11 @@ namespace Elements.Algorithms
 
         public void Insert(TKey key, TValue value)
         {
+            // insert a new number into the heap
+            // Asymptotic complexity: O(log n) worst case
+            //                        O(1)     average (assuming no Extract are intermixed)
+            //
+            // Algorithm: adds the element to the bottom layer and swaps it with its parents until the root is reached or swaps are no longer needed
             if (n < a.Count) a[n] = (key, value);
             else a.Add((key, value));
             int i = n++;
@@ -48,11 +63,16 @@ namespace Elements.Algorithms
 
         public void Insert((TKey, TValue) element)
         {
+            // just another version of the insert
             Insert(element.Item1, element.Item2);
         }
 
         public (TKey, TValue) Extract()
         {
+            // extracts and returns the top element from the heap
+            // Asymptotic complexity: O(log n) worst case
+            //
+            // Algorithm: swaps the top element with the last one, heapifies the very root
             (a[0], a[n - 1]) = (a[n - 1], a[0]);
             --n;
             heapify(0);
@@ -61,15 +81,21 @@ namespace Elements.Algorithms
 
         public void Clear()
         {
+            // empties the heap
             n = 0;
         }
 
+        // checks whether the heap is empty
         public bool Empty { get { return n == 0; } }
+        // returns the size of the heap
         public int Size { get { return n; } }
+        // returns the top element of the heap
         public (TKey, TValue) Max { get { return a[0]; } }
 
 
+        // current size of the heap
         private int n;
+        // the actual container for the heap elements
         private List<(TKey, TValue)> a;
     }
 }
