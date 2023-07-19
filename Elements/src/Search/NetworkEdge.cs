2b3a03cb83e21634ccd30217ccecc0b63589149b
srudenkoamc <srudenko@amcbridge.com>
2023-07-19 12:24:51 +0300

    Added NetworkEdge class
    NetworkEdge class is similar to LocalEdge class from Network.cs, but it uses NetworkNode instead if integer node index. NetworkEdge also doesn't have IsBetweenVertices method.

---
+++ Elements/src/Search/NetworkEdge.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+using Elements.Geometry;
+
+namespace Elements.Search
+{
+    internal class NetworkEdge
+    {
+        public NetworkNode Start { get; private set; }
+        public NetworkNode End { get; private set; }
+
+        public NetworkEdge(NetworkNode start, NetworkNode end)
+        {
+            visitDirections = VisitDirections.None;
+            Start = start;
+            End = end;
+        }
+
+        /// <summary>
+        /// Mark a vertex as having been visited from the specified index.
+        /// </summary>
+        /// <param name="start">The index of the vertex from which the edge is visited.</param>
+        public void MarkAsVisited(NetworkNode start)
+        {
+            if (Start.Equals(start))
+            {
+                visitDirections |= VisitDirections.Straight;
+            }
+            else if (End.Equals(start))
+            {
+                visitDirections |= VisitDirections.Opposite;
+            }
+        }
+
+        /// <summary>
+        /// Is this edge visited from the provided vertex?
+        /// </summary>
+        /// <param name="node">The node from which the vertex is visited.</param>
+        /// <returns>Returns true if the edge was visited from the vertex.</returns>
+        public bool IsVisitedFromVertex(NetworkNode node)
+        {
+            if (Start.Equals(node))
+            {
+                return visitDirections.HasFlag(VisitDirections.Straight);
+            }
+
+            if (End.Equals(node))
+            {
+                return visitDirections.HasFlag(VisitDirections.Opposite);
+            }
+
+            return false;
+        }
+
+        internal VisitDirections visitDirections;
+
+        public override string ToString()
+        {
+            return $"({Start.Id}; {End.Id})";
+        }
+    }
+}
