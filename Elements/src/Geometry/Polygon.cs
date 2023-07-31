008270f3975faece27df442974a54807d896de69
pederlh <36080334+pederlh@users.noreply.github.com>
2023-07-31 11:12:41 +0200

    Don't change using directive order

--- Elements/src/Geometry/Polygon.cs
+++ Elements/src/Geometry/Polygon.cs
@@ -1,11 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using ClipperLib;
 using Elements.Search;
 using Elements.Spatial;
 using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 
 namespace Elements.Geometry
 {
