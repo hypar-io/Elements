b624f2285884218376b5d5d0aa60fc580859a321
srudenkoamc <srudenko@amcbridge.com>
2023-08-28 18:27:22 +0300

    Fixed a comment

--- Elements/src/DoorOpeningType.cs
+++ Elements/src/DoorOpeningType.cs
@@ -6,8 +6,11 @@ namespace Elements
 {
     public enum DoorOpeningType
     {
+        [System.Runtime.Serialization.EnumMember(Value = @"Undefined")]
         Undefined,
+        [System.Runtime.Serialization.EnumMember(Value = @"Single Swing")]
         SingleSwing,
+        [System.Runtime.Serialization.EnumMember(Value = @"Double Swing")]
         DoubleSwing
     }
 }
