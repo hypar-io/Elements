898cfc11d0acc79cf56f9c57a82c9759bb228ad9
srudenkoamc <srudenko@amcbridge.com>
2023-09-07 16:12:30 +0300

    Added IIfcProductToElementConverter interface

---
+++ Elements.Serialization.IFC/src/Serialization/IFC/IFCToElementConverters/IIfcProductToElementConverter.cs
@@ -0,0 +1,13 @@
+﻿using IFC;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Elements.Serialization.IFC.Serialization.IFC.IFCToElementConverters
+{
+    internal interface IIfcProductToElementConverter
+    {
+        Element ConvertToElement(IfcProduct product, List<string> constructionErrors);
+        bool Matches(IfcProduct ifcProduct);
+    }
+}
