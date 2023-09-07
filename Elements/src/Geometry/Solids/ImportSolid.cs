def139010b6cd390923315b86296c73884db7676
srudenkoamc <srudenko@amcbridge.com>
2023-09-07 15:31:15 +0300

    Added ImportSolid : Solid Operation class

---
+++ Elements/src/Geometry/Solids/ImportSolid.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Elements.Geometry.Solids
+{
+    /// <summary>
+    /// A solid operation defined by an existing solid.
+    /// </summary>
+    public class ImportSolid : SolidOperation
+    {
+        /// <summary>
+        /// Create an import solid.
+        /// </summary>
+        /// <param name="solid">The solid which was imported.</param>
+        /// <param name="isVoid">Is the operation a void?</param>
+        public ImportSolid(Solid solid, bool isVoid = false) : base(isVoid)
+        {
+            _solid = solid;
+        }
+    }
+}
