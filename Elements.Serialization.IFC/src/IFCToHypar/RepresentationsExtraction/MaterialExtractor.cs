3135b488507cb98e701d46aea8d9689aed6d1e31
srudenkoamc <srudenko@amcbridge.com>
2023-09-21 05:53:41 +0300

    Material extractor refactoring

--- Elements.Serialization.IFC/src/IFCToHypar/RepresentationsExtraction/MaterialExtractor.cs
+++ Elements.Serialization.IFC/src/IFCToHypar/RepresentationsExtraction/MaterialExtractor.cs
@@ -11,7 +11,7 @@ namespace Elements.Serialization.IFC.IFCToHypar.RepresentationsExtraction
         public Dictionary<string, Material> MaterialByName { get; private set; }
         public Dictionary<Guid, Material> MaterialByGuid { get; private set; }
 
-        public MaterialExtractor(List<IfcStyledItem> styledItems)
+        public MaterialExtractor(IEnumerable<IfcStyledItem> styledItems)
         {
             MaterialByName = new Dictionary<string, Material>();
             MaterialByGuid = new Dictionary<Guid, Material>();
@@ -28,58 +28,66 @@ namespace Elements.Serialization.IFC.IFCToHypar.RepresentationsExtraction
             foreach (var styledItem in styledItems)
             {
                 var item = styledItem.Item; // The representation item that is styled.
+
                 if (item == null)
                 {
                     continue;
                 }
+
                 foreach (IfcStyleAssignmentSelect style in styledItem.Styles)
                 {
-                    if (style.Choice is IfcPresentationStyleAssignment styleAssign)
-                    {
-                        foreach (IfcPresentationStyleSelect presentationStyle in styleAssign.Styles)
-                        {
-                            if (presentationStyle.Choice is IfcSurfaceStyle surfaceStyle)
-                            {
-                                foreach (IfcSurfaceStyleElementSelect styleElement in surfaceStyle.Styles)
-                                {
-                                    if (styleElement.Choice is IfcSurfaceStyleRendering rendering)
-                                    {
-                                        var transparency = (IfcRatioMeasure)rendering.Transparency;
-                                        var color = rendering.SurfaceColour.ToColor(1.0 - transparency);
-                                        var name = surfaceStyle.Name;
+                    if (style.Choice is IfcPresentationStyle) {
+                        // See https://standards.buildingsmart.org/IFC/DEV/IFC4_2/FINAL/HTML/schema/ifcpresentationappearanceresource/lexical/ifcpresentationstyle.htm
+                        continue;
+                    }
 
-                                        Material material;
-                                        if (!MaterialByName.ContainsKey(name))
-                                        {
-                                            material = new Material(color,
-                                                                    0.4,
-                                                                    0.4,
-                                                                    false,
-                                                                    null,
-                                                                    false,
-                                                                    true,
-                                                                    null,
-                                                                    true,
-                                                                    null,
-                                                                    0.0,
-                                                                    false,
-                                                                    surfaceStyle.Id,
-                                                                    surfaceStyle.Name);
-                                            MaterialByName.Add(material.Name, material);
-                                        }
-                                        else
-                                        {
-                                            material = MaterialByName[name];
-                                        }
-                                        MaterialByGuid.Add(item.Id, material);
-                                    }
+                    if (!(style.Choice is IfcPresentationStyleAssignment styleAssign))
+                    {
+                        continue;
+                    }
+
+                    foreach (IfcPresentationStyleSelect presentationStyle in styleAssign.Styles)
+                    {
+                        if (!(presentationStyle.Choice is IfcSurfaceStyle surfaceStyle))
+                        {
+                            continue;
+                        }
+
+                        foreach (IfcSurfaceStyleElementSelect styleElement in surfaceStyle.Styles)
+                        {
+                            if (styleElement.Choice is IfcSurfaceStyleRendering rendering)
+                            {
+                                var transparency = (IfcRatioMeasure)rendering.Transparency;
+                                var color = rendering.SurfaceColour.ToColor(1.0 - transparency);
+                                var name = surfaceStyle.Name;
+
+                                Material material;
+                                if (!MaterialByName.ContainsKey(name))
+                                {
+                                    material = new Material(color,
+                                                            0.4,
+                                                            0.4,
+                                                            false,
+                                                            null,
+                                                            false,
+                                                            true,
+                                                            null,
+                                                            true,
+                                                            null,
+                                                            0.0,
+                                                            false,
+                                                            surfaceStyle.Id,
+                                                            surfaceStyle.Name);
+                                    MaterialByName.Add(material.Name, material);
                                 }
+                                else
+                                {
+                                    material = MaterialByName[name];
+                                }
+
+                                MaterialByGuid.Add(item.Id, material);
                             }
                         }
-                    }
-                    else if (style.Choice is IfcPresentationStyle)
-                    {
-                        // See https://standards.buildingsmart.org/IFC/DEV/IFC4_2/FINAL/HTML/schema/ifcpresentationappearanceresource/lexical/ifcpresentationstyle.htm
                     }
                 }
             }
