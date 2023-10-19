49c470df9068a8464ddca9d22f52b73c5b55926d
srudenkoamc <srudenko@amcbridge.com>
2023-10-19 15:52:42 +0300

    Added summaries to MappingInfo

--- Elements.Serialization.IFC/src/IFCToHypar/RepresentationsExtraction/MappingInfo.cs
+++ Elements.Serialization.IFC/src/IFCToHypar/RepresentationsExtraction/MappingInfo.cs
@@ -1,15 +1,21 @@
 ﻿using Elements.Geometry;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Elements.Serialization.IFC.IFCToHypar.RepresentationsExtraction
 {
+    /// <summary>Mapping information, extracted from IfcMappedItem representation.</summary>
     internal class MappingInfo
     {
+        /// <summary>Guid of MappingSource.MappedRepresentation of IfcMappedItem.</summary>
         public Guid MappingId { get; private set; }
+        /// <summary>The transform of the MappingSource of IfcMappedItem.</summary>
         public Transform MappingTransform { get; private set; }
 
+        /// <summary>
+        /// Create an object that contains the mapping information from IfcMappedItem.
+        /// </summary>
+        /// <param name="mappingId">Guid of MappingSource.MappedRepresentation of IfcMappedItem.</param>
+        /// <param name="mappingTransform">The transform of the MappingSource of IfcMappedItem.</param>
         public MappingInfo(Guid mappingId, Transform mappingTransform)
         {
             MappingId = mappingId;
