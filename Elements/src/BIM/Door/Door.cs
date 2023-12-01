6d8c7003505b023985a9db391d2bc45f1bb97f46
Anthonie Kramer <anthoniekramer@gmail.com>
2023-12-01 13:22:15 -0800

    handle visualization fix

--- Elements/src/BIM/Door/Door.cs
+++ Elements/src/BIM/Door/Door.cs
@@ -431,15 +431,15 @@ namespace Elements
 
                         if (OpeningSide == DoorOpeningSide.DoubleDoor)
                         {
-                                var handlePair1 = CreateHandlePair(-3 * 0.0254, false);
+                                var handlePair1 = CreateHandlePair(DoorWidth / 2 + 3 * 0.0254, false);
                                 solidOperationsList.AddRange(handlePair1);
 
-                                var handlePair2 = CreateHandlePair(3 * 0.0254, true);
+                                var handlePair2 = CreateHandlePair(DoorWidth / 2 + 3 * 0.0254, true);
                                 solidOperationsList.AddRange(handlePair2);
                         }
                         else if (OpeningSide != DoorOpeningSide.Undefined)
                         {
-                                var xPos = OpeningSide == DoorOpeningSide.LeftHand ? -(FullDoorWidthWithoutFrame / 2 - 2 * 0.0254) : (FullDoorWidthWithoutFrame / 2 - 2 * 0.0254);
+                                var xPos = OpeningSide == DoorOpeningSide.LeftHand ? (3 * 0.0254) : (3 * 0.0254);
                                 var handle = CreateHandlePair(xPos, OpeningSide == DoorOpeningSide.LeftHand);
                                 solidOperationsList.AddRange(handle);
                         }
@@ -450,14 +450,15 @@ namespace Elements
                         return repInst;
                 }
 
-                private List<SolidOperation> CreateHandlePair(double xRelPos, bool isCodirectionalToX)
+                private List<SolidOperation> CreateHandlePair(double handleOffset, bool isCodirectionalToX)
                 {
-                        var xOffset = xRelPos * DoorWidth * Vector3.XAxis;
-                        var yOffset = DoorThickness * Vector3.YAxis;
+                        var handleDir = isCodirectionalToX ? Vector3.XAxis : Vector3.XAxis.Negate();
+
+                        var xOffset = (-DoorWidth / 2 + handleOffset) * handleDir;
+                        var yOffset = DoorThickness / 2 * Vector3.YAxis;
                         var zOffset = HandleHeight * Vector3.ZAxis;
 
                         var solidOperationsList = new List<SolidOperation>();
-                        var handleDir = isCodirectionalToX ? Vector3.XAxis : Vector3.XAxis.Negate();
 
                         var handleOrigin1 = xOffset + yOffset + zOffset;
                         var handle1Ops = CreateHandle(handleOrigin1, handleDir, Vector3.YAxis);
@@ -474,9 +475,9 @@ namespace Elements
                 {
                         var circleTransform = new Transform(origin, handleDir, yDir);
                         var circle = new Circle(circleTransform, HandleBaseRadius).ToPolygon();
-                        var circleOperation = new Extrude(circle, 0.1 * HandleDepth, yDir);
+                        var circleOperation = new Extrude(circle, 0.1 * HandleDepth, yDir.Negate());
 
-                        var cyl1Transform = new Transform(origin + 0.1 * HandleDepth * yDir, handleDir, yDir);
+                        var cyl1Transform = new Transform(origin, handleDir, yDir);
                         var cyl1Circle = new Circle(cyl1Transform, HandleRadius).ToPolygon();
                         var cyl1Operation = new Extrude(cyl1Circle, 0.9 * HandleDepth, yDir);
 
